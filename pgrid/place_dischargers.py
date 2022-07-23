3ee87945c0084bf7826896dd1fbfcf78d3eac154
Aurora Leeson <auroral@uw.edu>
2022-07-22 19:49:30 -0700

    finalized a working and complete place_dischargers.py

--- pgrid/place_dischargers.py
+++ pgrid/place_dischargers.py
@@ -14,7 +14,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seawater as sw
 import pickle
-import time
 
 # load the default choices
 # dch = pickle.load(open(Gr['gdir'] / 'choices.p', 'rb'))
@@ -185,7 +184,7 @@ for wn in wwtp_df.index:
 
                         # check if new min_dist is smaller than previous min_dist
                         if min_dist_new < min_dist:
-                            # save min dist and grid cell index
+                            # save new min dist and grid cell index
                             min_dist = min_dist_new
                             psII = min_II
                             psJJ = min_JJ
@@ -203,85 +202,20 @@ for wn in wwtp_df.index:
                         # iterate into next level ring
                         ringnum += 1
 
-# --------------------------------------------------
-
-            # psII_list.append(ix)
-            # psJJ_list.append(iy)
-                
-            # # form unique entries of lists
-            # JI_ulist = []
-            # for pp in range(len(psII_list)):
-            #     ji_tup = (psJJ_list[pp], psII_list[pp])
-            #     if ji_tup not in JI_ulist:
-            #         JI_ulist.append(ji_tup)
-    
-            # # Then use the last two point to decide on the river direction.
-            # # dx and dy are in: [-1,0,1] and are interpreted as, e.g.
-            # # dx = 1 means that the source is on the E side of the rho cell
-            # # ( column +1 is the step we took to get to the last river channel rho cell).
-            # if len(JI_ulist) >1:
-            #     # simple if we have at least two points in JI_ulist
-            #     dx = JI_ulist[-1][1] - JI_ulist[-2][1] 
-            #     dy = JI_ulist[-1][0] - JI_ulist[-2][0]
-            # else:
-            #     # more tedious if we have ony one point in JI_ulist
-            #     dist, ang = sw.dist([y[0],y[-1]], [x[0], x[-1]])
-            #     if -45<ang and ang<=45:
-            #         dx=1; dy = 0
-            #     elif 45<ang and ang<=135:
-            #         dx=0; dy = 1
-            #     elif -135<ang and ang<=-45:
-            #         dx=0; dy = -1
-            #     elif ang<=-135 or ang>135:
-            #         dx=-1; dy = 0
-            #     else:
-            #         print(' Inconsistent angle for %s '.center(60,'*') % (dn))
-    
-            # # Write info for ROMS based on dx and dy.
-            # #
-            # # NOTE: the row_py and col_py that we write to river_info.csv use
-            # # python indexing conventions, meaning that the rho, u, and v grids
-            # # start at [0,0].  Hence when we plot things, as in plot_grid.py, we
-            # # should be able to use [row_py, col_py] directly with the u and v grids.
-            # #
-            # # However, when we later create the source positions for running ROMS,
-            # # e.g. in forcing/riv0/make_forcing_main.py => rivers.nc, we convert these
-            # # values to ROMS indexing conventions, meaning:
-            # # - add 1 to row of N/S sources
-            # # - add 1 to column of E/W sources.
-            # xoff = 0
-            # yoff = 0
-            # if dx==1 and dy==0:
-            #     # Source on E side of rho-cell
-            #     idir = 0 # 0 = E/W, 1 = N/S (redundant with 'uv')
-            #     isign = -1
-            #     uv = 'u'
-            # elif dx==-1 and dy==0:
-            #     # Source on W side of rho-cell
-            #     idir = 0
-            #     isign = 1
-            #     uv = 'u'
-            #     xoff = -1
-            # elif dx==0 and dy==1:
-            #     # Source on N side of rho-cell
-            #     idir = 1
-            #     isign = -1
-            #     uv = 'v'
-            # elif dx==0 and dy==-1:
-            #     # Source on S side of rho-cell
-            #     idir = 1
-            #     isign = 1
-            #     uv = 'v'
-            #     yoff = -1
-            # else:
-            #     print(' Inconsistent last points for %s '.center(60,'*') % (dn))
+            # NOTE: the row_py and col_py that we write to river_info.csv use
+            # python indexing conventions, meaning that the rho, u, and v grids
+            # start at [0,0].  Hence when we plot things, as in plot_grid.py, we
+            # should be able to use [row_py, col_py] directly with the u and v grids.
+            #
+            # However, when we later create the source positions for running ROMS,
+            # e.g. in forcing/riv0/make_forcing_main.py => rivers.nc, we convert these
+            # values to ROMS indexing conventions, meaning:
+            # - add 1 to row of N/S sources
+            # - add 1 to column of E/W sources.
 
             # save location of point source
-            roms_info_df.loc[wn, 'row_py'] = psII #JI_ulist[-1][0] + yoff
-            roms_info_df.loc[wn, 'col_py'] = psJJ #JI_ulist[-1][1] + xoff
-            # roms_info_df.loc[wn, 'idir'] = idir
-            # roms_info_df.loc[wn, 'isign'] = isign
-            # roms_info_df.loc[wn, 'uv'] = uv
+            roms_info_df.loc[wn, 'row_py'] = psII 
+            roms_info_df.loc[wn, 'col_py'] = psJJ 
         
         else:
             print(' >> excluding ' + wn.title())
@@ -289,17 +223,9 @@ for wn in wwtp_df.index:
     except FileNotFoundError:
         pass
 
-# # save the updated mask and z
-# ds.update({'mask_rho': (('eta_rho', 'xi_rho'), mask_rho)})
-# ds.update({'h': (('eta_rho', 'xi_rho'), -z)})
-# ds.to_netcdf(out_fn)
-# ds.close()
-
-# # save the roms river info
-# out_rfn = Gr['gdir'] / 'roms_river_info.csv'
-# print('\nCreating ' + str(out_rfn))
-# roms_info_df.index.name = 'rname'
-# roms_info_df.to_csv(out_rfn)
-# # here is how you would read the DataFrame back in
-# # df1 = pd.read_csv(out_rfn, index_col='rname')
+# save the roms point source info
+out_wfn = Gr['gdir'] / 'roms_wwtp_info.csv'
+print('\nCreating ' + str(out_wfn))
+roms_info_df.index.name = 'wname'
+roms_info_df.to_csv(out_wfn)
 
