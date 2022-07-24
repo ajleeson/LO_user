2a00950b0c4a8c4748226f674b29905fceb22312
Aurora Leeson <auroral@uw.edu>
2022-07-24 11:56:52 -0700

    working on debugging wwtp forcing

--- forcing/rivA0/make_forcing_main.py
+++ forcing/rivA0/make_forcing_main.py
@@ -1,9 +1,9 @@
 """
-This is the main program for making the WWTP forcing file.
+This is the main program for making the TIDE forcing file.
 
 Test on mac in ipython:
 
-run make_forcing_main.py -g awwtp1 -t v0 -r backfill -s continuation -d 2020.01.01 -f wwtp0 -test True
+run make_forcing_main.py -g ae0 -t v0 -r backfill -s continuation -d 2020.01.01 -f rivA0 -test True
 
 """
 
@@ -31,7 +31,7 @@ if Ldir['testing']:
     from importlib import reload
     reload(zrfun)
 
-out_fn = out_dir / 'wwtps.nc'
+out_fn = out_dir / 'rivers.nc'
 out_fn.unlink(missing_ok=True)
 
 # Make the time vector.  Here I just have two time points, at the start
@@ -49,10 +49,10 @@ N = S['N']
 grid_fn = Ldir['grid'] / 'grid.nc'
 G = zrfun.get_basic_info(grid_fn, only_G=True)
 
-# get the list of wwtps and indices for this grid
-gri_fn = Ldir['grid'] / 'wwtp_info.csv'
-gri_df = pd.read_csv(gri_fn, index_col='wname')
-NWWTP = len(gri_df)
+# get the list of rivers and indices for this grid
+gri_fn = Ldir['grid'] / 'river_info.csv'
+gri_df = pd.read_csv(gri_fn, index_col='rname')
+NRIV = len(gri_df)
 
 # Start Dataset
 ds = xr.Dataset()
@@ -63,12 +63,12 @@ ds['river_time'].attrs['units'] = Lfun.roms_time_units
 ds['river_time'].attrs['long_name'] = 'river time'
 
 # Add river coordinate
-ds['wwtp'] = (('wwtp',), np.arange(1,NWWTP+1))
-ds['wwtp'].attrs['long_name'] = 'point source identification number'
+ds['river'] = (('river',), np.arange(1,NRIV+1))
+ds['river'].attrs['long_name'] = 'river runoff identification number'
 
 # Add river names
-ds['wwtp_name'] = (('wwtp',), list(gri_df.index))
-ds['wwtp_name'].attrs['long_name'] = 'wwtp name'
+ds['river_name'] = (('river',), list(gri_df.index))
+ds['river_name'].attrs['long_name'] = 'river name'
 
 # Add Vshape
 vn = 'river_Vshape'
@@ -76,7 +76,7 @@ vinfo = zrfun.get_varinfo(vn, vartype='climatology')
 dims = ('s_rho', 'river')
 # For Vtransform = 2, even spacing is a good approximation, and
 # we implement this by using 1/N as the fraction in each vertical cell.
-Vshape = (1/N) * np.ones((N, NWWTP))
+Vshape = (1/N) * np.ones((N, NRIV))
 ds[vn] = (dims, Vshape)
 ds[vn].attrs['long_name'] = vinfo['long_name']
 
@@ -84,49 +84,46 @@ ds[vn].attrs['long_name'] = vinfo['long_name']
 for vn in ['river_Xposition', 'river_Eposition', 'river_direction']:
     vinfo = zrfun.get_varinfo(vn, vartype='climatology')
     if vn == 'river_direction':
-        wwtp_dir = [2.] # set point source diretion to enter vertically (2)
-        ds[vn] = (('wwtp',), wwtp_dir)
+        print('idir:{}'.format(gri_df.idir))
+        print('to numpy:{}'.format(gri_df.idir.to_numpy()))
+        ds[vn] = (('river',), gri_df.idir.to_numpy())
+        print('final:{}'.format(ds[vn]))
     elif vn == 'river_Xposition':
-        X_vec = np.nan * np.ones(NWWTP)
+        X_vec = np.nan * np.ones(NRIV)
         ii = 0
-        for wn in gri_df.index:
-            # if gri_df.loc[wn, 'idir'] == 0:
-            #     X_vec[ii] = gri_df.loc[wn, 'col_py'] + 1
-            # elif gri_df.loc[wn, 'idir'] == 1:
-            #     X_vec[ii] = gri_df.loc[wn, 'col_py']
-            # Kind of just added this case, but need to double check !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-            X_vec[ii] = gri_df.loc[wn, 'col_py']
+        for rn in gri_df.index:
+            if gri_df.loc[rn, 'idir'] == 0:
+                X_vec[ii] = gri_df.loc[rn, 'col_py'] + 1
+            elif gri_df.loc[rn, 'idir'] == 1:
+                X_vec[ii] = gri_df.loc[rn, 'col_py']
             ii += 1
-        ds[vn] = (('wwtp',), X_vec)
+        ds[vn] = (('river',), X_vec)
     elif vn == 'river_Eposition':
-        E_vec = np.nan * np.ones(NWWTP)
+        E_vec = np.nan * np.ones(NRIV)
         ii = 0
-        for wn in gri_df.index:
-            # if gri_df.loc[wn, 'idir'] == 0:
-            #     E_vec[ii] = gri_df.loc[wn, 'row_py']
-            # elif gri_df.loc[wn, 'idir'] == 1:
-            #     E_vec[ii] = gri_df.loc[wn, 'row_py'] + 1
-            # Kind of just added this case, but need to double check !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-            E_vec[ii] = gri_df.loc[wn, 'row_py']
+        for rn in gri_df.index:
+            if gri_df.loc[rn, 'idir'] == 0:
+                E_vec[ii] = gri_df.loc[rn, 'row_py']
+            elif gri_df.loc[rn, 'idir'] == 1:
+                E_vec[ii] = gri_df.loc[rn, 'row_py'] + 1
             ii += 1
-        ds[vn] = (('wwtp',), E_vec)
+        ds[vn] = (('river',), E_vec)
     ds[vn].attrs['long_name'] = vinfo['long_name']
         
 # Add transport
 vn = 'river_transport'
 vinfo = zrfun.get_varinfo(vn, vartype='climatology')
-dims = (vinfo['time'],) + ('wwtp',)
-Q_mat = np.zeros((NT, NWWTP))
+dims = (vinfo['time'],) + ('river',)
+Q_mat = np.zeros((NT, NRIV))
 ii = 0
-for wn in gri_df.index:
-    if wn == 'creek0':
-        Q_mat[:,ii] = 1000 * np.ones(NT) * gri_df.loc[wn, 'isign']
+for rn in gri_df.index:
+    if rn == 'creek0':
+        Q_mat[:,ii] = 1000 * np.ones(NT) * gri_df.loc[rn, 'isign']
         # You could make the transport a function of time, for example by making
         # dti = pd.DatetimeIndex([dt0, dt1]) and then using a function of
         # dti.dayofyear.
     else:
         # You could add other rivers here
-        Q_mat[:,ii] = 1000 * np.ones(NT)
         pass
     ii += 1
 ds[vn] = (dims, Q_mat)
@@ -138,9 +135,9 @@ for vn in ['river_salt', 'river_temp']:
     vinfo = zrfun.get_varinfo(vn, vartype='climatology')
     dims = (vinfo['time'],) + ('s_rho', 'river')
     if vn == 'river_salt':
-        TR_mat = np.zeros((NT, N, NWWTP))
+        TR_mat = np.zeros((NT, N, NRIV))
     elif vn == 'river_temp':
-        TR_mat = 10 * np.ones((NT, N, NWWTP))
+        TR_mat = 10 * np.ones((NT, N, NRIV))
     ds[vn] = (dims, TR_mat)
     ds[vn].attrs['long_name'] = vinfo['long_name']
     ds[vn].attrs['units'] = vinfo['units']
