1600b73b692d8c972457cf4b606b63531391257f
Aurora Leeson <auroral@uw.edu>
2022-07-29 16:11:34 -0700

    working on section plot

--- plotting/roms_plots.py
+++ plotting/roms_plots.py
@@ -1114,7 +1114,7 @@ def P_sect_alpe2(in_dict):
         lat = G['lat_rho']
         zdeep = -30
 
-        x = np.linspace(1.04*lat.min(), 0.975*lat.max(), 500)
+        x = np.linspace(-1.4, 1.4, 500)
         y = 45.06 * np.ones(x.shape)
 
     v2, v3, dist, idist0 = pfun.get_section(ds, vn, x, y, in_dict)
