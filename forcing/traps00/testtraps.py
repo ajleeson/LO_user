223b50a5fbd9497d261359b35a213c083ddd7402
Aurora Leeson <auroral@uw.edu>
2022-10-09 22:48:18 -0700

    TRAPS placement algorithm

---
+++ forcing/traps00/testtraps.py
@@ -0,0 +1,8 @@
+import trapsfun
+from importlib import reload
+reload(trapsfun)
+
+# run testtraps.py -g cas6 -t v0 -r backfill -s continuation -d 2010.01.01 -f traps00 -test True
+
+result = trapsfun.traps_placement('riv') # riv or wwtp
+print(result)
