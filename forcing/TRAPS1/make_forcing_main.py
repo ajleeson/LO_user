56ed46b808b9c1a563b310335665930fb68345aa
Aurora Leeson <auroral@uw.edu>
2022-12-29 20:46:38 -0800

    consolidated rivers that discharge to same cell

--- forcing/TRAPS1/make_forcing_main.py
+++ forcing/TRAPS1/make_forcing_main.py
@@ -3,7 +3,7 @@ This is the main program for making the RIVER and TRAPS forcing file, for the
 updated ROMS
 
 Test on pc in ipython:
-run make_forcing_main.py -g cas6 -r backfill -d 2021.01.01 -f TRAPS0 -test True
+run make_forcing_main.py -g cas6 -r backfill -d 2021.01.01 -f TRAPS1 -test True
 
 """
 
@@ -236,8 +236,26 @@ if enable_tinyrivers == True:
     gri_fn = Ldir['grid'] / 'triv_info.csv'
     gri_df = pd.read_csv(gri_fn, index_col='rname')
     if Ldir['testing']:
-        gri_df = gri_df.loc[['Birch Bay', 'North Olympic'],:]
-    NTRIV = len(gri_df)
+        gri_df = gri_df.loc[['Birch Bay', 'Purdy Cr', 'Burley Cr', 'Perry Cr','McLane Cr'],:]
+
+    # get list of overlapping rivers
+    overlapping_trivs = gri_df[gri_df.duplicated(['row_py','col_py'], keep=False) == True].index.values
+    # consolidate overlapping rivers
+    combined_names = trapsfun.combine_adjacent(overlapping_trivs)
+    gri_df_no_ovrlp = pd.DataFrame(columns=gri_df.columns) 
+    gri_df_no_ovrlp.index.name='rname'
+    for trname in gri_df.index: # loop through original dataframe
+        if trname in overlapping_trivs: # look for rivers that are in the list of duplicates
+            name_index = np.where(overlapping_trivs == trname)[0][0] # get index in the list of duplicates
+            if name_index%2 == 0: # even index means first occurence of duplicate
+                newname = combined_names[int(name_index/2)] # combine names of duplicates
+                gri_df_no_ovrlp.loc[newname] = gri_df.loc[trname] # add combined source to dataframe
+            # Note: second duplicate will be dropped (so idir, isign, and uv will come from the first duplicate)
+        else:
+            gri_df_no_ovrlp.loc[trname] = gri_df.loc[trname] # if not a duplicate, then just copy over original info
+
+    NTRIV = len(gri_df_no_ovrlp)
+    # NTRIV = len(gri_df)
 
     # get the flow, temperature, and nutrient data for these days
     qtbio_triv_df_dict = trapsfun.get_qtbio(gri_df, dt_ind, yd_ind, Ldir, traps_type)
@@ -265,25 +283,25 @@ if enable_tinyrivers == True:
     for vn in ['river_Xposition', 'river_Eposition', 'river_direction']:
         vinfo = zrfun.get_varinfo(vn, vartype='climatology')
         if vn == 'river_direction':
-            triv_ds[vn] = (('river',), gri_df.idir.to_numpy())
+            triv_ds[vn] = (('river',), gri_df_no_ovrlp.idir.to_numpy())
         elif vn == 'river_Xposition':
             X_vec = np.nan * np.ones(NTRIV)
             ii = 0
-            for rn in gri_df.index:
-                if gri_df.loc[rn, 'idir'] == 0:
-                    X_vec[ii] = gri_df.loc[rn, 'col_py'] + 1
-                elif gri_df.loc[rn, 'idir'] == 1:
-                    X_vec[ii] = gri_df.loc[rn, 'col_py']
+            for rn in gri_df_no_ovrlp.index:
+                if gri_df_no_ovrlp.loc[rn, 'idir'] == 0:
+                    X_vec[ii] = gri_df_no_ovrlp.loc[rn, 'col_py'] + 1
+                elif gri_df_no_ovrlp.loc[rn, 'idir'] == 1:
+                    X_vec[ii] = gri_df_no_ovrlp.loc[rn, 'col_py']
                 ii += 1
             triv_ds[vn] = (('river',), X_vec)
         elif vn == 'river_Eposition':
             E_vec = np.nan * np.ones(NTRIV)
             ii = 0
-            for rn in gri_df.index:
-                if gri_df.loc[rn, 'idir'] == 0:
-                    E_vec[ii] = gri_df.loc[rn, 'row_py']
-                elif gri_df.loc[rn, 'idir'] == 1:
-                    E_vec[ii] = gri_df.loc[rn, 'row_py'] + 1
+            for rn in gri_df_no_ovrlp.index:
+                if gri_df_no_ovrlp.loc[rn, 'idir'] == 0:
+                    E_vec[ii] = gri_df_no_ovrlp.loc[rn, 'row_py']
+                elif gri_df_no_ovrlp.loc[rn, 'idir'] == 1:
+                    E_vec[ii] = gri_df_no_ovrlp.loc[rn, 'row_py'] + 1
                 ii += 1
             triv_ds[vn] = (('river',), E_vec)
         triv_ds[vn].attrs['long_name'] = vinfo['long_name']
@@ -294,10 +312,22 @@ if enable_tinyrivers == True:
     dims = (vinfo['time'],) + ('river',)
     Q_mat = np.zeros((NT, NTRIV))
     rr = 0
-    for rn in gri_df.index:
-        qtbio_triv_df = qtbio_triv_df_dict[rn]
-        flow = qtbio_triv_df['flow'].values
-        Q_mat[:,rr] = flow * gri_df.loc[rn, 'isign']
+    for rn in gri_df_no_ovrlp.index:
+        # sum flowrates together if duplicate river
+        if '&' in rn:
+            # split into individual rivers
+            [riv1,riv2] = rn.split('&')
+            # get individual river flowrates
+            qtbio_triv_df_1 = qtbio_triv_df_dict[riv1]
+            qtbio_triv_df_2 = qtbio_triv_df_dict[riv2]
+            flow1 = qtbio_triv_df_1['flow'].values
+            flow2 = qtbio_triv_df_2['flow'].values
+            # combine river flow
+            flow = flow1 + flow2
+        else:
+            qtbio_triv_df = qtbio_triv_df_dict[rn]
+            flow = qtbio_triv_df['flow'].values
+        Q_mat[:,rr] = flow * gri_df_no_ovrlp.loc[rn, 'isign']
         rr += 1
     triv_ds[vn] = (dims, Q_mat)
     triv_ds[vn].attrs['long_name'] = vinfo['long_name']
@@ -312,13 +342,23 @@ if enable_tinyrivers == True:
         elif vn == 'river_temp':
             TS_mat = np.nan * np.zeros((NT, N, NTRIV))
             rr = 0
-            for rn in gri_df.index:
-                qtbio_triv_df = qtbio_triv_df_dict[rn]
+            for rn in gri_df_no_ovrlp.index:
+                if '&' in rn:
+                    # split into individual rivers
+                    [riv1,riv2] = rn.split('&')
+                    # get individual river dataframe
+                    qtbio_triv_df_1 = qtbio_triv_df_dict[riv1]
+                    qtbio_triv_df_2 = qtbio_triv_df_dict[riv2]
+                    # calculate weighted average
+                    temps = trapsfun.weighted_average('temp',qtbio_triv_df_1, qtbio_triv_df_2)
+                else:
+                    qtbio_triv_df = qtbio_triv_df_dict[rn]
+                    temps = qtbio_triv_df['temp'].values
                 for nn in range(N):
-                    TS_mat[:, nn, rr] = qtbio_triv_df['temp'].values
+                    TS_mat[:, nn, rr] = temps
                 rr += 1
         if np.isnan(TS_mat).any():
-            print('Error from traps00: nans in tiny river river_temp!')
+            print('Error from traps: nans in tiny river river_temp!')
             sys.exit()
         triv_ds[vn] = (dims, TS_mat)
         triv_ds[vn].attrs['long_name'] = vinfo['long_name']
@@ -331,13 +371,23 @@ if enable_tinyrivers == True:
         dims = (vinfo['time'],) + ('s_rho', 'river')
         B_mat = np.nan * np.zeros((NT, N, NTRIV))
         rr = 0
-        for rn in gri_df.index:
-            qtbio_triv_df = qtbio_triv_df_dict[rn]
+        for rn in gri_df_no_ovrlp.index:
+            if '&' in rn:
+                # split into individual rivers
+                [riv1,riv2] = rn.split('&')
+                # get individual river dataframe
+                qtbio_triv_df_1 = qtbio_triv_df_dict[riv1]
+                qtbio_triv_df_2 = qtbio_triv_df_dict[riv2]
+                # calculate weighted average
+                bvals = trapsfun.weighted_average(var,qtbio_triv_df_1, qtbio_triv_df_2)
+            else:
+                qtbio_triv_df = qtbio_triv_df_dict[rn]
+                bvals = qtbio_triv_df[var].values
             for nn in range(N):
-                B_mat[:, nn, rr] = qtbio_triv_df[var].values
+                B_mat[:, nn, rr] = bvals
             rr += 1
         if np.isnan(TS_mat).any():
-            print('Error from traps00: nans in tiny river bio!')
+            print('Error from traps: nans in tiny river bio!')
             sys.exit()
         triv_ds[vn] = (dims, B_mat)
         triv_ds[vn].attrs['long_name'] = vinfo['long_name']
@@ -352,13 +402,13 @@ if enable_tinyrivers == True:
         dims = (vinfo['time'],) + ('s_rho', 'river')
         B_mat = np.nan * np.zeros((NT, N, NTRIV))
         rr = 0
-        for rn in gri_df.index:
-            qtbio_triv_df = qtbio_triv_df_dict[rn]
+        for rn in gri_df_no_ovrlp.index:
+            # qtbio_triv_df = qtbio_triv_df_dict[rn]
             for nn in range(N):
                 B_mat[:, nn, rr] = rivfun.get_bio_vec(bvn, rn, yd_ind)
             rr += 1
         if np.isnan(B_mat).any():
-            print('Error from traps00: nans in B_mat for tiny river ' + vn)
+            print('Error from traps: nans in B_mat for tiny river ' + vn)
             sys.exit()
         triv_ds[vn] = (dims, B_mat)
         triv_ds[vn].attrs['long_name'] = vinfo['long_name']
@@ -366,12 +416,12 @@ if enable_tinyrivers == True:
 
     # Rename rivers that share name with WWTP. This code appends ' R' at the end of the river name
     duplicates = ['Port Angeles', 'Port Townsend', 'Birch Bay', 'Port Gamble', 'Gig Harbor']
-    print(gri_df.index)
-    gri_df.index = np.where(gri_df.index.isin(duplicates), gri_df.index + ' R', gri_df.index)
-    print(gri_df.index) 
+    print(gri_df_no_ovrlp.index)
+    gri_df_no_ovrlp.index = np.where(gri_df_no_ovrlp.index.isin(duplicates), gri_df_no_ovrlp.index + ' R', gri_df_no_ovrlp.index)
+    print(gri_df_no_ovrlp.index) 
 
     # Add river names
-    triv_ds['river_name'] = (('river',), list(gri_df.index))
+    triv_ds['river_name'] = (('river',), list(gri_df_no_ovrlp.index))
     triv_ds['river_name'].attrs['long_name'] = 'tiny river name'
 
 ###########################################################################################
