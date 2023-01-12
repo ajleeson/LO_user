33c077daaf4704be91686d06bb821f4322ba00ef
Aurora Leeson <auroral@uw.edu>
2023-01-11 19:55:49 -0800

    test without wwtps

--- forcing/TRAPS5/overlapping_traps.py
+++ forcing/TRAPS5/overlapping_traps.py
@@ -19,11 +19,7 @@ import pandas as pd
 import rivfun
 import trapsfun
 
-# get the list of pre-existing rivers and indices for this grid
-lo_fn = Ldir['grid'] / 'river_info.csv'
-lo_df = pd.read_csv(lo_fn, index_col='rname')
-
-# get the list of tiny rivers and indices for this grid
+# get the list of rivers and indices for this grid
 tr_fn = Ldir['grid'] / 'triv_info.csv'
 tr_df = pd.read_csv(tr_fn, index_col='rname')
 
@@ -33,8 +29,7 @@ ps_df = pd.read_csv(ps_fn, index_col='rname')
 
 # merge the dataframes
 traps_df = tr_df.append(ps_df)
-all_df = traps_df.append(lo_df)
 
 # get list of sources that discharge to the same cell (overlapping)
-duplicate_df = all_df[all_df.duplicated(['row_py','col_py'], keep=False) == True]
+duplicate_df = traps_df[traps_df.duplicated(['row_py','col_py'], keep=False) == True]
 print(duplicate_df)
