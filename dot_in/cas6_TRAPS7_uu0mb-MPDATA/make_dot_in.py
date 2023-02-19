89c8cb04b798453e3952b59719ff85f83fd7daa3
Aurora Leeson <auroral@uw.edu>
2023-02-19 14:54:27 -0800

    test Akima 4th order advection

--- dot_in/cas6_TRAPS7_uu0mb-MPDATA/make_dot_in.py
+++ dot_in/cas6_TRAPS7_uu0mb-MPDATA/make_dot_in.py
@@ -169,7 +169,7 @@ if Ldir['short_roms']:
 else:
     D['ntimes'] = int(days_to_run*86400/dtsec)
 
-D['ninfo'] = 1 #int(his_interval/dtsec) # how often to write info to the log file (# of time steps)
+D['ninfo'] = int(his_interval/dtsec) # how often to write info to the log file (# of time steps)
 D['nhis'] = int(his_interval/dtsec) # how often to write to the history files
 D['ndefhis'] = D['nhis'] # how often to create new history files
 D['nrst'] = int(rst_interval*86400/dtsec)
