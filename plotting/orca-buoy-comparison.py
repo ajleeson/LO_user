853649f200251c2235990c8f8e9f18a1fd4e7486
Aurora Leeson <auroral@uw.edu>
2023-07-19 11:18:12 -0700

    orca buoy DO comparison

--- plotting/orca-buoy-comparison.py
+++ plotting/orca-buoy-comparison.py
@@ -87,29 +87,36 @@ distances_m = zfun.ll2xy(lon1,lat1,lon0,lat0)
 x_dist_km = round(distances_m[0]/1000)
 ax.plot([lon0,lon1],[lat0,lat1],color='k',linewidth=5)
 ax.text(lon0,lat0+0.01,'{} km'.format(x_dist_km),color='k',fontsize=14)
+# plot buoy locations
+buoy_names = ['CI','PW','NB','DB','HP','TW']
+buoy_lon = [-122.7300,-122.3972,-122.6270,-122.8029,-123.1126,-123.0083]
+buoy_lat = [47.2800,47.7612,47.9073,47.8034,47.4218,47.3750]
+ax.scatter(buoy_lon,buoy_lat,s=100,color='deeppink')
+for i,name in enumerate(buoy_names):
+    ax.text(buoy_lon[i]-0.04,buoy_lat[i]+0.02,name,fontsize='16',fontweight='bold',color='black')
 
 # Loop through and add Orca Buoy data and model output ----------------------------------------
 
 # get orca data
-orca_nc = ['TW_ds.nc','PW_ds.nc','NB_ds.nc','HP_ds.nc','DB_ds.nc','CI_ds.nc']
+orca_nc = ['TW','PW','NB','HP','DB','CI']
 # fig numbers
 fig_no = [3,4,7,8,11,12]
 # titles
 titles = ['TW - Twanoh','PW - Point Wells','NB - North Buoy', 'HP - Hoodsport', 'DB - Dabob Bay', 'CI - Carr Inlet']
 
-# read in data model data at penn cove
-ds_penn_withDIN = xr.open_dataset('/home/aleeson/LO_output/extract/cas6_traps2_x2b/moor/pennlynch/PennCove_2017.01.01_2017.12.30.nc')
-DO_penn_withDIN = ds_penn_withDIN['oxygen'][:,0]*pinfo.fac_dict['oxygen'] # bottom DO
-ds_penn_base = xr.open_dataset('/home/aleeson/LO_output/extract/cas6_traps3_x2b/moor/pennlynch/PennCove_2017.01.01_2017.12.31.nc')
-DO_penn_base = ds_penn_base['oxygen'][:,0]*pinfo.fac_dict['oxygen'] # bottom DO
-# get time vectors
-time_withDIN = ds_penn_withDIN['ocean_time']
-time_base = ds_penn_base['ocean_time']
-
 # loop through all of the stations
 for i,orca in enumerate(orca_nc):
     # create subplot
     ax = fig.add_subplot(3,4,fig_no[i])
+    # Add baseline and create timevector
+    ds_baseline = xr.open_dataset('/home/aleeson/LO_output/extract/cas6_traps3_x2b/moor/orca/'+orca+'_2017.01.01_2017.12.31.nc')
+    DO_baseline = ds_baseline['oxygen'][:,0]*pinfo.fac_dict['oxygen'] # bottom DO
+    year_time = ds_baseline['ocean_time']
+    ax.plot(year_time,DO_baseline,linestyle='-',color='mediumturquoise',linewidth=2, label='Baseline [no WW discharge]')
+    # Add with WWTP DIN case
+    ds_WWTPDIN = xr.open_dataset('/home/aleeson/LO_output/extract/cas6_traps2_x2b/moor/orca/'+orca+'_2017.01.01_2017.12.31.nc')
+    DO_WWTPDIN = ds_WWTPDIN['oxygen'][:,0]*pinfo.fac_dict['oxygen'] # bottom DO
+    ax.plot(year_time,DO_WWTPDIN,linestyle='--',color='k',linewidth=1,label='With WW discharge')
     # add title
     ax.set_title(titles[i] + ' Bottom DO [mg/L]',fontsize=16)
     # format y labels
@@ -119,7 +126,7 @@ for i,orca in enumerate(orca_nc):
     else:
         plt.yticks(fontsize=12)
     # format x labels
-    ax.set_xlim([time_base[0] - np.timedelta64(1,'D'),time_base[-1]])
+    ax.set_xlim([year_time[0] - np.timedelta64(1,'D'),year_time[-1]])
     ax.xaxis.set_major_locator(MonthLocator())
     ax.grid(True,color='w',linewidth=2)
     if i < 4:
@@ -132,50 +139,20 @@ for i,orca in enumerate(orca_nc):
     ax.set_facecolor('#EEEEEE')
     for border in ['top','right','bottom','left']:
         ax.spines[border].set_visible(False)
-    # ax.plot(time_base,DO_lynch_base,linestyle='-',color='mediumturquoise',linewidth=2, label='Baseline [no WW discharge]')
-    # ax.plot(time_withDIN,DO_lynch_withDIN,linestyle='--',color='k',linewidth=1,label='With WW discharge')
     # add observations
-    ds_orca = xr.open_dataset('/home/aleeson/LO_data/obs/ORCA/LO_orca_moor/datasets/'+orca)
+    ds_orca = xr.open_dataset('/home/aleeson/LO_data/obs/ORCA/LO_orca_moor/datasets/'+orca+'_ds.nc')
     orca_time = ds_orca.time.values
     orca_do = ds_orca.oxy.values[:,-1]
     ax.plot(orca_time,orca_do,'o',color='darkmagenta',markersize=6,alpha=0.3,label='Observations*')
     # add legend
     if i == 0:
         ax.legend(loc='upper right',fontsize=12)
-
-
-# # Lynch Cove
-# ds_lynch_withDIN = xr.open_dataset('/home/aleeson/LO_output/extract/cas6_traps2_x2b/moor/pennlynch/LynchCove_2017.01.01_2017.12.30.nc')
-# DO_lynch_withDIN = ds_lynch_withDIN['oxygen'][:,0]*pinfo.fac_dict['oxygen'] # bottom DO
-# ds_lynch_base = xr.open_dataset('/home/aleeson/LO_output/extract/cas6_traps3_x2b/moor/pennlynch/LynchCove_2017.01.01_2017.12.31.nc')
-# DO_lynch_base = ds_lynch_base['oxygen'][:,0]*pinfo.fac_dict['oxygen'] # bottom DO
-# ax = fig.add_subplot(3,4,3)
-# ax.set_title('TW - Twanoh Bottom DO [mg/L]',fontsize=16)
-# ax.set_ylim([0,12])
-# ax.set_xticklabels([])
-# ax.set_xlim([time_base[0] - np.timedelta64(1,'D'),time_base[-1]])
-# ax.plot(time_base,DO_lynch_base,linestyle='-',color='mediumturquoise',linewidth=2, label='Baseline [no WW discharge]')
-# ax.plot(time_withDIN,DO_lynch_withDIN,linestyle='--',color='k',linewidth=1,label='With WW discharge')
-# # Add orca buoy data
-# ds_orca = xr.open_dataset('/home/aleeson/LO_data/obs/ORCA/LO_orca_moor/datasets/TW_ds.nc')
-# orca_time = ds_orca.time.values
-# orca_do = ds_orca.oxy.values[:,-1]
-# ax.plot(orca_time,orca_do,'o',color='darkmagenta',markersize=6,alpha=0.5,label='Observations*')
-# ax.legend(loc='upper right',fontsize=12)
-# ax.set_facecolor('#EEEEEE')
-# for border in ['top','right','bottom','left']:
-#     ax.spines[border].set_visible(False)
-# ax.tick_params(axis='x', colors='w')
-# ax.xaxis.set_major_locator(MonthLocator())
-# ax.grid(True,color='w',linewidth=2)
-# plt.yticks(fontsize=14)
-# # add water depth
-# ax.text(time_base[15],0.5,'depth = {} m'.format(round(-1*np.mean(ds_lynch_base['z_rho'][:,0].values),1)),fontsize=12)
-
+    # add water depth
+    ax.text(year_time[15],0.5,'depth = {} m'.format(round(-1*np.mean(ds_baseline['z_rho'][:,0].values),1)),fontsize=12)
 
 
 # Generate plot
+plt.suptitle('LiveOcean and ORCA buoy bottom DO comparison',fontweight='bold',fontsize=20)
 plt.tight_layout
-plt.subplots_adjust(left=0.05, bottom=0.05, right=0.95, top=0.95, wspace=0.05, hspace=0.3)
+plt.subplots_adjust(left=0.05, bottom=0.05, right=0.95, top=0.90, wspace=0.05, hspace=0.3)
 plt.savefig('orca_buoy_plot.png')
-# plt.show()
