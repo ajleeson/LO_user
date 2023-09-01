142070e312b97d0495ee019f9c18fd93709a415a
Aurora Leeson <auroral@uw.edu>
2023-09-01 10:07:41 -0700

    point to Parkers LO_data to look for hycom data

--- forcing/ocnVFC/Ofun.py
+++ forcing/ocnVFC/Ofun.py
@@ -12,6 +12,7 @@ from scipy.spatial import cKDTree
 import seawater
 import subprocess
 import requests
+from pathlib import Path
 
 import Ofun_CTD
 from lo_tools import Lfun, zfun, zrfun
@@ -202,7 +203,8 @@ def get_hnc_short_list(this_dt, Ldir):
     elif this_dt >= datetime(2018,12,7):
         hy_list = hy_list[ihy_split:]
     # create a list of daily HYCOM NetCDF files in the archive
-    hy_in_dir = Ldir['data'] / 'hycom'
+    # hy_in_dir = Ldir['data'] / 'hycom'
+    hy_in_dir = Path('/data1/parker/LO_data/hycom')
     hnc_list = []
     for hy in hy_list:
         in_dir = hy_in_dir / hy
