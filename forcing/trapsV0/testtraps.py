1b3e1e6115fc9cfde483c5dad5e900e47b5861e3
Aurora Leeson <auroral@uw.edu>
2023-09-15 16:26:51 -0700

    cleaning old forcing files

--- forcing/trapsV0/testtraps.py
+++
@@ -1,8 +0,0 @@
-import trapsfun
-from importlib import reload
-reload(trapsfun)
-
-# run testtraps.py -g cas6 -r backfill -s continuation -d 2021.01.01 -f TRAPS3 -test True
-
-trapsfun.traps_placement('riv')
-trapsfun.traps_placement('wwtp')
