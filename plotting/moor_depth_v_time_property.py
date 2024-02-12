bae104c5e18288210bc86e93ed2f401192590178
Aurora Leeson <auroral@uw.edu>
2024-02-11 19:24:50 -0800

    cleaned up old figures and made new figure to look at depth v time property plots

---
+++ plotting/moor_depth_v_time_property.py
@@ -0,0 +1,207 @@
+"""
+Generate depth vs. time property plots using mooring extraction data. 
+Used to compare the with-loading run to the no-loading run.
+"""
+
+from subprocess import Popen as Po
+from subprocess import PIPE as Pi
+from matplotlib.markers import MarkerStyle
+import matplotlib.dates as mdates
+import numpy as np
+import xarray as xr
+from datetime import datetime, timedelta
+import pandas as pd
+import cmocean
+import matplotlib.pylab as plt
+import gsw
+import pinfo
+
+from lo_tools import Lfun, zfun, zrfun
+from lo_tools import plotting_functions as pfun
+
+Ldir = Lfun.Lstart()
+
+##########################################################
+##                    Define inputs                     ##
+##########################################################
+
+gtagexes = ['cas7_t0noN_x4b', 'cas7_t0_x4b']
+jobname = 'noWWTPNtest'
+startdate = '2013.01.01'
+enddate = '2013.12.31'
+year = '2013' # for making a date label
+
+# gtagexes = gtagexes[0:1]
+
+vn_list = ['rho', 'NO3', 'NH4', 'oxygen', 'phytoplankton']
+
+# figure settings
+fs = 14 # figure font size
+ls = 12 # label size
+ts = 16 # title size
+
+##########################################################
+##              Get stations and gtagexes               ##
+##########################################################
+
+# parse gtagex
+gtagexample = gtagexes[0]
+gridname, tag, ex_name = gtagexample.split('_')
+Ldir = Lfun.Lstart(gridname=gridname, tag=tag, ex_name=ex_name)
+
+# find job lists from the extract moor
+job_lists = Lfun.module_from_file('job_lists', Ldir['LOu'] / 'extract' / 'moor' / 'job_lists.py')
+
+# Get mooring stations:
+sta_dict = job_lists.get_sta_dict(jobname)
+
+##########################################################
+##                      Plotting                        ##
+##########################################################
+
+plt.close('all')
+
+z_max = 6 # upper vertical limit (to see top of sea surface)
+
+# Loop through all of the mooring stations
+for i,station in enumerate(sta_dict.keys()):
+
+    # calculate lat/lon for station
+    lon = sta_dict[station][0]
+    lat = sta_dict[station][1]
+
+    # Initialize Figure
+    fig, ax = plt.subplots(5,2,figsize = (18,9), sharex = True, sharey = True)
+    fig.suptitle(station, fontsize = ts)
+    
+    # loop through different state variables
+    for j,vn in enumerate(vn_list):
+
+        # loop through two model runs
+        for gtagex in gtagexes:
+
+            # no loading run
+            if gtagex == 'cas7_t0noN_x4b':
+                title = 'Natural'
+                # download .nc files
+                fn = '../../LO_output/extract/' + gtagex + '/moor/' + jobname + '/' + station + '_' + startdate + '_' + enddate + '.nc'
+                ds = xr.open_dataset(fn)
+                # get depth values
+                z_rho = ds['z_rho'].transpose() # depth of u and v-velocities
+                z_w   = ds['z_w'].transpose()   # depth of w-velocities
+                z_min = np.min(z_w.values)
+                # column number
+                col = 0
+                # calculate density
+                if vn == 'rho':
+                    # Calculate density
+                    ds = ds.assign(p=gsw.p_from_z(ds['z_rho'],lat))
+                    # calculate absolute salinity from practical salinity
+                    ds = ds.assign(salt_abs=gsw.conversions.SA_from_SP(ds['salt'], ds['z_rho'], lon, lat))
+                    # calculate conservative temperature from potential temperature
+                    ds = ds.assign(temp_cons=gsw.conversions.CT_from_pt(ds['salt_abs'], ds['temp']))
+                    # calculate density
+                    ds = ds.assign(rho=gsw.rho(ds['salt_abs'],ds['temp_cons'],ds['p']))
+                    # set scale and units
+                    scale = 1
+                    units = ' $(kg\ m^{-3})$'
+                    vmin = 1015
+                    vmax = 1025
+                    cmap = 'jet'
+                else:
+                    # get scale and units
+                    scale =  pinfo.fac_dict[vn]
+                    units = pinfo.units_dict[vn]
+                    vlims = pinfo.vlims_dict[vn]
+                    vmin = vlims[0]
+                    vmax = vlims[1]
+                    cmap = pinfo.cmap_dict[vn]
+                # get dataset
+                val = ds[vn].transpose() * scale
+
+            elif gtagex == 'cas7_t0_x4b':
+                title = 'Anthropogenic minus Natural'
+                # download .nc files
+                fn = '../../LO_output/extract/' + gtagex + '/moor/' + jobname + '/' + station + '_' + startdate + '_' + enddate + '.nc'
+                ds_withLoading = xr.open_dataset(fn)
+                # get depth values
+                z_rho = ds_withLoading['z_rho'].transpose() # depth of u and v-velocities
+                z_w   = ds_withLoading['z_w'].transpose()   # depth of w-velocities
+                z_min = np.min(z_w.values)
+                # column number
+                col = 1
+                # calculate density
+                if vn == 'rho':
+                    # Calculate density
+                    ds_withLoading = ds_withLoading.assign(p=gsw.p_from_z(ds_withLoading['z_rho'],lat))
+                    # calculate absolute salinity from practical salinity
+                    ds_withLoading = ds_withLoading.assign(salt_abs=gsw.conversions.SA_from_SP(ds_withLoading['salt'], ds_withLoading['z_rho'], lon, lat))
+                    # calculate conservative temperature from potential temperature
+                    ds_withLoading = ds_withLoading.assign(temp_cons=gsw.conversions.CT_from_pt(ds_withLoading['salt_abs'], ds_withLoading['temp']))
+                    # calculate density
+                    ds_withLoading = ds_withLoading.assign(rho=gsw.rho(ds_withLoading['salt_abs'],ds_withLoading['temp_cons'],ds_withLoading['p']))
+                    # set scale and units
+                    scale = 1
+                    units = ' $(kg\ m^{-3})$'
+                else:
+                    # get scale and units
+                    scale =  pinfo.fac_dict[vn]
+                    units = pinfo.units_dict[vn]
+                    vlims = pinfo.vlims_dict[vn]
+                # caculatate difference between runs
+                ds_withLoading = ds_withLoading.assign(t0_minus_t0noN=(ds_withLoading[vn]-ds[vn]))
+                val = ds_withLoading['t0_minus_t0noN'].transpose() * scale
+                # get min and max for plotting
+                factor = 3
+                vmin = np.nanmin(val.values,axis=0)
+                vmin = factor*np.nanmean(vmin)
+                vmax = np.nanmax(val.values,axis=0)
+                vmax = factor*np.nanmean(vmax)
+                # make sure colorbar axis contains zero
+                if vmin > 0 and vmax > 0:
+                    vmin = vmax*-1.01
+                if vmin < 0 and vmax < 0:
+                    vmax = vmin*-1.01
+                if vmin == 0 and vmax == 0:
+                    vmin = -0.11
+                    vmax = 0.1
+                cmap = cmocean.tools.crop(cmocean.cm.balance_r, vmin, vmax, 0)
+
+            # create time vector
+            dates = pd.date_range(start= startdate, end= enddate, freq= '1d')
+            dates_local = [pfun.get_dt_local(x) for x in dates]
+
+            # Plot pcolormesh
+            cs = ax[j,col].pcolormesh(dates_local, z_rho, val, vmin=vmin, vmax=vmax, cmap=cmap)#cmocean.cm.haline))
+            cbar = fig.colorbar(cs, ax=ax[j,col], aspect=10)
+            cbar.outline.set_visible(False)
+            if col == 0:
+                ax[j,col].set_ylabel('z (m)', fontsize = fs)
+            ax[j,col].text(0.05, 0.05, vn + units, fontweight='bold',
+                    verticalalignment='bottom', horizontalalignment='left',
+                    transform=ax[j,col].transAxes, fontsize=ls)
+            ax[j,col].tick_params(axis='both', which='major', labelsize=ls)
+            ax[j,col].set_ylim((z_min,z_max))
+            ax[j,col].grid(True,color='k',linewidth=1,linestyle=':',axis='x')
+
+            # add bottom axis
+            if j == 4:
+                ax[j,col].set_xlabel('2013', fontsize = fs)
+                ax[j,col].tick_params(axis='both', which='major', labelsize=ls)
+                ax[j,col].xaxis.set_major_formatter(mdates.DateFormatter("%b"))
+                ax[j,col].tick_params(axis='x', labelrotation=30, labelsize=ls)
+
+            # add title
+            ax[0,col].set_title(title,fontsize=ts)
+
+            # add note about colorbar
+            ax[0,1].text(0.95, 0.7, 'Anthropogenic higher',color='royalblue',
+                    verticalalignment='bottom', horizontalalignment='right',
+                    transform=ax[0,1].transAxes, fontsize=fs)
+            ax[0,1].text(0.95, 0.5, 'Natural higher',color='crimson',
+                    verticalalignment='bottom', horizontalalignment='right',
+                    transform=ax[0,1].transAxes, fontsize=fs)
+
+    plt.tight_layout
+    plt.subplots_adjust(left=0.05, right=0.95, top=0.90, wspace=0.02)
+    plt.show()
--- plotting/mooring_analysis.py
+++ plotting/mooring_analysis.py
@@ -35,6 +35,8 @@ if str(pth) not in sys.path:
 import gfun_utility as gfu
 import gfun
 
+Ldir = Lfun.Lstart()
+
 #%% DEFINE INPUTS ----------------------------------------------------------------------------
 
 gtagexes = ['cas7_t0noN_x4b', 'cas7_t0_x4b']
@@ -48,6 +50,10 @@ year = '2013' # for making a date label
 #%%  PLOT THE MOORING LOCATIONS -----------------------------------------
 
 plt.close('all')
+
+# where to put output figures
+out_dir = Ldir['LOo'] / 'AL_custom_plots' / 'mooring_timeseries'
+Lfun.make_dir(out_dir)
 
 # parse gtagex
 gtagexample = gtagexes[0]
@@ -112,24 +118,17 @@ for i,station in enumerate(sta_dict.keys()):
              c='deeppink', horizontalalignment='center', fontweight = 'bold')
 
 
-#%% CREATE THE TIMESERIES --------------------------------------------------------------------
+#%% Chlorophyll and DO --------------------------------------------------------------------
 
 # create time vector
 dates = pd.date_range(start= startdate, end= enddate, freq= '1d')
 dates_local = [pfun.get_dt_local(x) for x in dates]
 
-# create colors list
-n = len(gtagexes)
-print(n)
-# colors = cmocean.cm.thermal(np.linspace(0.1, 0.8, n))
-
 # Loop through all of the mooring stations
 for i,station in enumerate(sta_dict.keys()):
 
-    print('creating new plots')
-
     # initalize a new figure
-    fig = plt.figure(figsize=(10,8))
+    fig = plt.figure(figsize=(10,8))#,facecolor='none')
     plt.tight_layout()
     fig.suptitle(station)
 
@@ -160,16 +159,16 @@ for i,station in enumerate(sta_dict.keys()):
         # save datasets for later, and add titles
         if gtagex == 'cas7_t0noN_x4b':
             ds_noN = ds
-            title = 'N-less run'
+            title = 'No-loading'
         elif gtagex == 'cas7_t0_x4b':
             ds_hindcast = ds
-            title = 'with WWTP N'
+            title = 'With-loading'
 
         # add subtitles for each panel
         axN[j].text(0.02, 0.8, letters[j]+title, fontweight='bold',
             verticalalignment='bottom', horizontalalignment='left',
-            transform=axN[0].transAxes, fontsize=12)
-        axN[2].text(0.02, 0.8, '(c) Difference (with N minus N-less)', fontweight='bold',
+            transform=axN[j].transAxes, fontsize=12)
+        axN[2].text(0.02, 0.8, '(c) With-loading minus No-loading', fontweight='bold',
             verticalalignment='bottom', horizontalalignment='left',
             transform=axN[2].transAxes, fontsize=12)
 
@@ -182,7 +181,7 @@ for i,station in enumerate(sta_dict.keys()):
         units = pinfo.units_dict['phytoplankton']
         phyto_val = ds.phytoplankton.values[:,surf_lev] * scale
         axN[j].plot(dates_local, phyto_val,color=phyto_color,linewidth=2)
-        axN[j].tick_params(axis='y', labelcolor=phyto_color)
+        axN[j].tick_params(axis='y', labelcolor=phyto_color, labelsize=12, color=phyto_color)
         axN[j].set_ylim([0,35])
         axN[j].set_xlim([dates_local[0],dates_local[-1]])
         axN[j].set_xticklabels([])
@@ -201,7 +200,7 @@ for i,station in enumerate(sta_dict.keys()):
             diff = phyto_val_hindcast - phyto_val_noN
             # plot difference
             axN[j+1].plot(dates_local, diff, color = phyto_color,linewidth=2)
-            axN[j+1].tick_params(axis='y', labelcolor=phyto_color)
+            axN[j+1].tick_params(axis='y', labelcolor=phyto_color, labelsize=12, color=phyto_color)
             # pick limits
             lim = 1.2*(max(abs(min(diff)),max(diff)))
             axN[j+1].set_ylim([-1*lim,lim])
@@ -213,7 +212,7 @@ for i,station in enumerate(sta_dict.keys()):
         units = pinfo.units_dict['oxygen']
         oxy_val = ds.oxygen.values[:,bott_lev] * scale
         axDO[j].plot(dates_local, oxy_val, color = oxy_color,linewidth=2)
-        axDO[j].tick_params(axis='y', labelcolor=oxy_color)
+        axDO[j].tick_params(axis='y', labelcolor=oxy_color, labelsize=12, color = oxy_color)
         axDO[j].set_ylim([0,12])
         axDO[j].set_xlim([dates_local[0],dates_local[-1]])
         axDO[j].set_xticklabels([])
@@ -230,7 +229,7 @@ for i,station in enumerate(sta_dict.keys()):
             diff = oxy_val_hindcast - oxy_val_noN
             # plot difference
             axDO[j+1].plot(dates_local, diff, color = oxy_color,linewidth=2)
-            axDO[j+1].tick_params(axis='y', labelcolor=oxy_color)
+            axDO[j+1].tick_params(axis='y', labelcolor=oxy_color, labelsize=12, color = oxy_color)
             # pick limits
             lim = 1.2*(max(abs(min(diff)),max(diff)))
             axDO[j+1].set_ylim([-1*lim,lim])
@@ -238,9 +237,9 @@ for i,station in enumerate(sta_dict.keys()):
 
         # format date label
         axDO[2].xaxis.set_major_formatter(mdates.DateFormatter("%b"))
-        axDO[2].set_xlabel('2013')
-        axDO[2].tick_params(axis='x', labelrotation=30)
-        axN[2].tick_params(axis='x', labelrotation=30)
+        axDO[2].set_xlabel('2013')#, color='#EEEEEE')
+        axDO[2].tick_params(axis='x', labelrotation=30, labelsize=12) # colors='#EEEEEE'
+        axN[2].tick_params(axis='x', labelrotation=30, labelsize=12) # colors='#EEEEEE'
 
         # format subplot colors
         for ax in axN + axDO:
@@ -249,6 +248,122 @@ for i,station in enumerate(sta_dict.keys()):
             for border in ['top','right','bottom','left']:
                 ax.spines[border].set_visible(False)
 
+    plt.savefig(out_dir / (station+'_chl_do.png'))#, facecolor=ax.get_facecolor(), transparent=True)
 
-#%% GENERATE PLOTS ------------------------------------------------------------------------------
-plt.show()
+
+#%% Total water column N --------------------------------------------------------------------
+                
+# Loop through all of the mooring stations
+for i,station in enumerate(sta_dict.keys()):
+
+    # get multiplier
+    scale =  pinfo.fac_dict['NO3'] # mmol/m3
+
+    # initalize a new figure
+    fig = plt.figure(figsize=(10,6),facecolor='none')
+    plt.tight_layout()
+    fig.suptitle(station)
+
+    # loop through all of the model scenarios
+    for gtagex in gtagexes:
+        # download .nc files
+        fn = '../../LO_output/extract/' + gtagex + '/moor/' + jobname + '/' + station + '_' + startdate + '_' + enddate + '.nc'
+        ds = xr.open_dataset(fn)
+        # save datasets for later, and add titles
+        if gtagex == 'cas7_t0noN_x4b':
+            ds_noN = ds
+        elif gtagex == 'cas7_t0_x4b':
+            ds_hindcast = ds
+
+    # get NO3 and NH4 arrays (in mmol/m3)
+    NO3_noN = ds_noN['NO3'].values * scale # dimensions of time, s_rho
+    NH4_noN = ds_noN['NH4'].values * scale # dimensions of time, s_rho
+    DIN_noN = NO3_noN + NH4_noN # dimensions of time, s_rho
+    NO3_hindcast = ds_hindcast['NO3'].values * scale # dimensions of time, s_rho
+    NH4_hindcast = ds_hindcast['NH4'].values * scale # dimensions of time, s_rho
+    DIN_hindcast = NO3_hindcast + NH4_hindcast # dimensions of time, s_rho
+
+    # get depth of sigma layers
+    zw_noN = ds_noN['z_w'] # dimensions of time, s_w
+    zw_hindcast = ds_hindcast['z_w'] # dimensions of time, s_w
+
+    # get total depth at mooring location
+    depth_noN = zw_noN[:,-1] - zw_noN[:,0] # dimensions of time
+    depth_hindcast = zw_hindcast[:,-1] - zw_hindcast[:,0] # dimensions of time
+
+    # calculate thickness of sigma layers
+    thick_noN = np.diff(zw_noN.values) # dimensions of time, s_rho
+    thick_hindcast = np.diff(zw_hindcast.values) # dimensions of time, s_rho
+
+    # multiply cell N concentration by cell thickness and area (500x500m) (mmol)
+    molNO3_noN = NO3_noN * thick_noN * 500 * 500
+    molNO3_hindcast = NO3_hindcast * thick_hindcast * 500 * 500
+    molNH4_noN = NH4_noN * thick_noN * 500 * 500
+    molNH4_hindcast = NH4_hindcast * thick_hindcast * 500 * 500
+    molDIN_noN = DIN_noN * thick_noN * 500 * 500
+    molDIN_hindcast = DIN_hindcast * thick_hindcast * 500 * 500
+    
+    # sum throughout water column to get total N in water column (kg where 1 mmol = 7.1378E-8 kg)
+    TmolNO3_noN = np.sum(molNO3_noN,axis=1) * 7.1378E-8
+    TmolNO3_hindcast = np.sum(molNO3_hindcast,axis=1) * 7.1378E-8
+    TmolNH4_noN = np.sum(molNH4_noN,axis=1) * 7.1378E-8
+    TmolNH4_hindcast = np.sum(molNH4_hindcast,axis=1) * 7.1378E-8
+    TmolDIN_noN = np.sum(molDIN_noN,axis=1) * 7.1378E-8
+    TmolDIN_hindcast = np.sum(molDIN_hindcast,axis=1) * 7.1378E-8
+    
+    # plot total water column N of the two runs
+    ax = plt.subplot(211)
+    # NO3
+    # ax.plot(dates_local, TmolNO3_noN, color='deeppink',linewidth=0.6,label='NO3 N-less')
+    # ax.plot(dates_local, TmolNO3_hindcast, color='deeppink',linewidth=2,alpha=0.6,label='NO3 with-N')
+    # # NH4
+    # ax.plot(dates_local, TmolNH4_noN, color='mediumorchid',linewidth=0.6,label='NH4 N-less')
+    # ax.plot(dates_local, TmolNH4_hindcast, color='mediumorchid',linewidth=2,alpha=0.6,label='NH4 with-N')
+    # DIN
+    ax.plot(dates_local, TmolDIN_noN, color='darkcyan',linewidth=0.6,label='No-loading')
+    ax.plot(dates_local, TmolDIN_hindcast, color='darkcyan',linewidth=2.5,alpha=0.4,label='With-loading')
+    # format subplot
+    ax.set_xlim([dates_local[0],dates_local[-1]])
+    ax.grid(True,color='w',linewidth=2)
+    ax.set_facecolor('#EEEEEE')
+    ax.set_xticklabels([])
+    ax.tick_params(axis='y', labelcolor='#EEEEEE', labelsize=12, color='#EEEEEE')
+    ax.set_ylim([0,1.2*max(TmolDIN_hindcast)])
+    for border in ['top','right','bottom','left']:
+        ax.spines[border].set_visible(False)
+    ax.legend(loc='lower left')
+    ax.text(0.02, 0.8, '(a) Depth-integrated DIN [kg]', fontweight='bold',
+            verticalalignment='bottom', horizontalalignment='left',
+            transform=ax.transAxes, fontsize=12)
+    
+    # plot the difference between the two runs
+    ax = plt.subplot(212)
+    # # NO3
+    # ax.plot(dates_local, TmolNO3_hindcast-TmolNO3_noN, color='deeppink',linewidth=2,label='NO3')
+    # # NH4
+    # ax.plot(dates_local, TmolNH4_hindcast-TmolNH4_noN, color='mediumorchid',linewidth=2,label='NH4')
+    # DIN
+    ax.plot(dates_local, TmolDIN_hindcast-TmolDIN_noN, color='darkcyan',linewidth=2,label='DIN')
+    # format subplot
+    ax.set_xlim([dates_local[0],dates_local[-1]])
+    ax.grid(True,color='w',linewidth=2)
+    ax.tick_params(axis='y', labelcolor='#EEEEEE', labelsize=12, color='#EEEEEE')
+    ax.set_facecolor('#EEEEEE')
+    for border in ['top','right','bottom','left']:
+        ax.spines[border].set_visible(False)
+    # ax.legend(loc='lower left')
+    ax.text(0.02, 0.8, '(b) With-loading minus No-loading', fontweight='bold',
+            verticalalignment='bottom', horizontalalignment='left',
+            transform=ax.transAxes, fontsize=12)
+
+    plt.suptitle(station)
+    # format date label
+    ax.xaxis.set_major_formatter(mdates.DateFormatter("%b"))
+    ax.set_xlabel('2013', color='#EEEEEE',fontsize=12)
+    ax.tick_params(axis='x', labelrotation=30, colors='#EEEEEE', labelsize=12)
+                
+    plt.savefig(out_dir / (station+'_depth_integrated_N.png'))
+
+
+# #%% GENERATE PLOTS ------------------------------------------------------------------------------
+# plt.show()
