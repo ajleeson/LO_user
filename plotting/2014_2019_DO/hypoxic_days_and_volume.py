656994e1a854c53a6631b2476c66779abd5ae1d9
ajleeson <aurorajleeson@gmail.com>
2024-05-20 15:00:28 -0700

    update figures for research

--- plotting/2014_2019_DO/hypoxic_days_and_volume.py
+++ plotting/2014_2019_DO/hypoxic_days_and_volume.py
@@ -14,6 +14,7 @@ import xarray as xr
 from datetime import datetime, timedelta
 from matplotlib.dates import DateFormatter
 from matplotlib.dates import MonthLocator
+import matplotlib.patches as patches
 from matplotlib.offsetbox import (OffsetImage, AnnotationBbox)
 import matplotlib.image as image
 import pandas as pd
@@ -48,7 +49,7 @@ DO_thresh = 2 # [mg/L]
 # Show WWTP locations?
 WWTP_loc = True
 
-remove_straits = True
+remove_straits = False
 
 vn = 'oxygen'
 
@@ -212,8 +213,6 @@ for year in years:
     # get timeseries of hypoxic volume (by summing over spatial dimensions)
     hyp_thick_timeseries = np.nansum(hyp_thick,axis=1)
     hyp_thick_timeseries = np.nansum(hyp_thick_timeseries,axis=1)
-    print(hyp_thick_timeseries.shape)
-    print(hyp_thick_timeseries)
     # calculate hypoxic volume
     # area of grid cell = 0.5 km by 0.5 km times thickness of hypoxic layer (converted from m to km)
     hyp_vol_timeseries = 0.5 * 0.5 * (hyp_thick_timeseries/1000) 
@@ -327,7 +326,7 @@ enddate = '2020.12.31'
 dates = pd.date_range(start= startdate, end= enddate, freq= '1d')
 dates_local = [pfun.get_dt_local(x) for x in dates]
 
-colors = ['red','darkorange','gold','green','blue','purple']
+colors = ['red','darkorange','gold','green','blue','purple','deeppink']
 
 # plot timeseries
 for i,year in enumerate(years):
@@ -354,9 +353,45 @@ plt.savefig(out_dir / 'area_with_DO_lt{}'.format(DO_thresh))
 
 # initialize figure
 plt.close('all)')
-pfun.start_plot(figsize=(12,5))
-fig,ax = plt.subplots(1,1)
+pfun.start_plot(figsize=(16,8))
+f, (ax0, ax1) = plt.subplots(1, 2, gridspec_kw={'width_ratios': [1, 3]})
 
+# format figure
+ax0.set_xlim([xmin,xmax])
+ax0.set_ylim([ymin,ymax])
+ax0.set_yticklabels([])
+ax0.set_xticklabels([])
+# ax0.axis('off')
+pfun.dar(ax0)
+pfun.add_coast(ax0)
+# Create a Rectangle patch to omit Straits
+if remove_straits:
+    # get lat and lon
+    fp = Ldir['LOo'] / 'extract' / gtagex / 'box' / ('pugetsoundDO_2013.01.01_2013.12.31.nc')
+    ds = xr.open_dataset(fp)
+    lons = ds.coords['lon_rho'].values
+    lats = ds.coords['lat_rho'].values
+    lon = lons[0,:]
+    lat = lats[:,0]
+    # Straits
+    lonmax = -122.76
+    lonmin = xmin
+    latmax = ymax
+    latmin = 48.14
+    # convert lat/lon to eta/xi
+    diff = np.absolute(lon-lonmin)
+    ximin = diff.argmin()
+    diff = np.absolute(lon-lonmax)
+    ximax = diff.argmin()
+    diff = np.absolute(lat-latmin)
+    etamin = diff.argmin()
+    diff = np.absolute(lat-latmax)
+    etamax = diff.argmin()
+    rect = patches.Rectangle((lon[ximin], lat[etamin]), lon[ximax]-lon[ximin], lat[etamax]-lat[etamin],
+                            edgecolor='none', facecolor='gray', alpha=0.5)
+    # Add the patch to the Axes
+    ax0.add_patch(rect)
+    ax0.text(-123.2,48.25,'Straits\nomitted')
 
 # create time vector
 startdate = '2020.01.01'
@@ -364,23 +399,21 @@ enddate = '2020.12.31'
 dates = pd.date_range(start= startdate, end= enddate, freq= '1d')
 dates_local = [pfun.get_dt_local(x) for x in dates]
 
-colors = ['red','darkorange','gold','green','blue','purple']
-
 # plot timeseries
 for i,year in enumerate(years):
     # plot hypoxic area timeseries
-    plt.plot(dates_local,hyp_vol[year],color=colors[i],
+    ax1.plot(dates_local,hyp_vol[year],color=colors[i],
              linewidth=3,alpha=0.5,label=year)
 
 # format figure
-ax.grid(visible=True, color='w')
+ax1.grid(visible=True, color='w')
 # format background color
-ax.set_facecolor('#EEEEEE')
+ax1.set_facecolor('#EEEEEE')
 for border in ['top','right','bottom','left']:
-    ax.spines[border].set_visible(False)
-ax.xaxis.set_major_formatter(mdates.DateFormatter("%b"))
+    ax1.spines[border].set_visible(False)
+ax1.xaxis.set_major_formatter(mdates.DateFormatter("%b"))
 plt.legend(loc='best')
-plt.title('Hypoxic volume (DO < 2 mg/L) ' + r'[km$^3$]')
-ax.set_xlim([dates_local[0],dates_local[-1]])
+plt.title('Puget Sound hypoxic volume (DO < 2 mg/L) ' + r'[km$^3$]')
+ax1.set_xlim([dates_local[0],dates_local[-1]])
 
 plt.savefig(out_dir / 'volume_with_DO_lt2')
