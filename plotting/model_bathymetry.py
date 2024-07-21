278d42654e0a6fefd7c0df74d164c2d9cb7fe684
ajleeson <aurorajleeson@gmail.com>
2024-07-21 13:47:28 -0700

    updat plotting code

--- plotting/model_bathymetry.py
+++ plotting/model_bathymetry.py
@@ -13,13 +13,15 @@ Ldir = Lfun.Lstart()
 
 # define grid indices to look at
 j1 = 570
-j2 = 1200
+j2 = 1220
 i1 = 250
 i2 = 652
 
 # where to put output figures
 out_dir = Ldir['LOo'] / 'AL_custom_plots'
 Lfun.make_dir(out_dir)
+
+background = 'dimgray'
 
 # Get LiveOcean grid info --------------------------------------------------
 
@@ -41,24 +43,30 @@ zm[np.transpose(mask_rho) == 0] = np.nan
 # Initialize figure
 plt.close('all')
 fs = 10
-pfun.start_plot(fs=fs, figsize=(12,7))
+pfun.start_plot(fs=fs, figsize=(10,7))
 fig = plt.figure()
 plt.subplots_adjust(wspace=0, hspace=0)
 # create colormap
 # newcmap = cmocean.tools.crop_by_percent(cmocean.cm.deep_r, 20, which='max', N=None)
-newcmap = cmocean.cm.deep_r
-newcmap.set_bad('silver',1.) # background color
+# newcmap = cmocean.cm.deep_r
+newcmap = cmocean.tools.crop_by_percent(cmocean.cm.deep_r, 10, which='max')
+newcmap.set_bad(background,1.) # background color
+
+zm[np.transpose(mask_rho) != 0] = -1
+newcmap = plt.get_cmap('Blues_r')
+newcmap.set_bad(background,1.)
 
 # Salish Sea ----------------------------------------------------------
 ax0 = fig.add_subplot(1,2,1)
-cs = ax0.pcolormesh(plon, plat, zm, vmin=-600, vmax=0, cmap=newcmap)
-cbar = plt.colorbar(cs,ax=ax0, location='left')
-cbar.ax.tick_params(labelsize=14, rotation=30)
-cbar.ax.set_ylabel('Depth [m]', fontsize=14)
-cbar.outline.set_visible(False)
+cs = ax0.pcolormesh(plon, plat, zm, vmin=-5, vmax=0, cmap=newcmap)
+# cs = ax0.pcolormesh(plon, plat, zm, vmin=-500, vmax=0, cmap=newcmap)
+# cbar = plt.colorbar(cs,ax=ax0, location='right')
+# cbar.ax.tick_params(labelsize=11)#, rotation=30)
+# cbar.ax.set_ylabel('Depth [m]', fontsize=11)
+# cbar.outline.set_visible(False)
 # format figure
 pfun.dar(ax0)
-pfun.add_coast(ax0, color='gray')
+# pfun.add_coast(ax0, color='gray')
 for border in ['top','right','bottom','left']:
         ax0.spines[border].set_visible(False)
 # Set axis limits
@@ -82,32 +90,54 @@ x_dist_km = round(distances_m[0]/1000)
 ax0.plot([lon0,lon1],[lat0,lat1],color='k',linewidth=5)
 ax0.text((lon0+lon1)/2,lat0+0.05,'{} km'.format(x_dist_km),color='k',fontsize=12,
          horizontalalignment='center')
-# # draw box around Puget Sound
-# ax0.add_patch(Rectangle((-123.2, 46.93), 1.1, 1.52,
-#              edgecolor = 'blueviolet', facecolor = 'none', lw=2))
+# draw box around Puget Sound
+bordercolor = 'black'
+ax0.add_patch(Rectangle((-123.2, 46.93), 1.1, 1.52,
+             edgecolor = bordercolor, facecolor='none', lw=1))
+
+# add major cities
+# Seattle
+ax0.scatter([-122.3328],[47.6061],s=[250],color='lightpink',
+            marker='*',edgecolors='darkred')
+ax0.text(-122.3328 + 0.1,47.6061,'Seattle',color='white', rotation=90,
+         horizontalalignment='left',verticalalignment='center', size=12)
+# Vancouver
+ax0.scatter([-123.1207],[49.2827],s=[250],color='lightpink',
+            marker='*',edgecolors='darkred')
+ax0.text(-123.1207 + 0.1,49.2827,'Vancouver',color='white', rotation=0,
+         horizontalalignment='left',verticalalignment='center', size=12)
+
+# add major water bodies
+ax0.text(-124.937095,47.827238,'Pacific Ocean',color='black', rotation=-75,
+         horizontalalignment='left',verticalalignment='center', size=12)
 
 # Puget Sound ----------------------------------------------------------
 ax1 = fig.add_subplot(1,2,2)
-cs = ax1.pcolormesh(plon, plat, zm, vmin=-250, vmax=0, cmap=newcmap)
-# # add isobath
-# cl = ax1.contour(lon, lat, zm, [-100], colors='k',
-#                  linestyles='solid', linewidths=1)
-# ax0.clabel(cl, inline=True, fontsize=10)
-# add colorbar
-cbar = plt.colorbar(cs,ax=ax1, location='right')
-cbar.ax.tick_params(labelsize=14, rotation=30)
-cbar.ax.set_ylabel('Depth [m]', fontsize=14)
-cbar.outline.set_visible(False)
+zm = z.copy()
+zm[np.transpose(mask_rho) == 0] = np.nan
+zm[np.transpose(mask_rho) != 0] = -1
+newcmap = plt.get_cmap('Blues_r')
+newcmap.set_bad(background,1.)
+cs = ax1.pcolormesh(plon, plat, zm, vmin=-5, vmax=0, cmap=newcmap)
+# cs = ax1.pcolormesh(plon, plat, zm, vmin=-250, vmax=0, cmap=newcmap)
+# cbar = plt.colorbar(cs,ax=ax1, location='right')
+# cbar.ax.tick_params(labelsize=11)#, rotation=30)
+# cbar.ax.set_ylabel('Depth [m]', fontsize=11)
+# cbar.outline.set_visible(False)
 # format figure
 pfun.dar(ax1)
-pfun.add_coast(ax1, color='gray')
+# pfun.add_coast(ax1, color='gray')
 for border in ['top','right','bottom','left']:
-        ax1.spines[border].set_visible(False)
+        # ax1.spines[border].set_visible(False)
+        ax1.spines[border].set_color(bordercolor)
+        ax1.spines[border].set_linewidth(2)
 # Set axis limits
 ax1.set_xlim([-123.3,-122.1])
 ax1.set_ylim([46.93,48.45])
-plt.xticks(rotation=30, color='gray')
-plt.yticks(color='gray')
+ax1.set_yticklabels([])
+ax1.set_xticklabels([])
+# plt.xticks(rotation=30, color='gray')
+# plt.yticks(color='gray')
 # add title
 ax1.set_title('Puget Sound',fontsize=16)
 # add 10 km bar
@@ -117,8 +147,9 @@ lat1 = lat0
 lon1 = -123.06825
 distances_m = zfun.ll2xy(lon1,lat1,lon0,lat0)
 x_dist_km = round(distances_m[0]/1000)
-ax1.plot([lon0,lon1],[lat0,lat1],color='k',linewidth=5)
-ax1.text((lon0+lon1)/2,lat0+0.015,'{} km'.format(x_dist_km),color='k',fontsize=12,
+ax1.plot([lon0,lon1],[lat0,lat1],color='w',linewidth=5)
+ax1.text((lon0+lon1)/2,lat0+0.015,'{} km'.format(x_dist_km),color='w',fontsize=12,
          horizontalalignment='center')
 
+plt.subplots_adjust(hspace = 0.01)
 plt.savefig(out_dir / ('model_bathy.png'))
