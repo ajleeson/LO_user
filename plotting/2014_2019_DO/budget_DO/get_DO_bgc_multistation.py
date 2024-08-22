56f80269755e1c97296841dae330466dd74df958
ajleeson <aurorajleeson@gmail.com>
2024-08-22 12:00:08 -0700

    dec

--- plotting/2014_2019_DO/budget_DO/get_DO_bgc_multistation.py
+++ plotting/2014_2019_DO/budget_DO/get_DO_bgc_multistation.py
@@ -63,8 +63,11 @@ Ldir['gtagex'] = 'cas7_t0_x4b'
 # ds0 = '2014.10.01'
 # ds1 = '2014.10.31'
 # nov
-ds0 = '2014.11.01'
-ds1 = '2014.11.30'
+# ds0 = '2014.11.01'
+# ds1 = '2014.11.30'
+# dec
+ds0 = '2014.12.01'
+ds1 = '2014.12.31'
 
 # where to put output figures
 out_dir = Ldir['LOo'] / 'pugetsound_DO' / ('DO_budget_'+ds0+'_'+ds1) / '2layer_bgc'
