ca7e5cb26ccee6f18a58ec904ed135884ed3d6a2
ajleeson <aurorajleeson@gmail.com>
2025-07-07 15:46:56 -0700

    figs for noise tests

--- plotting/bit-reproducibility/model_bit_diff.py
+++ plotting/bit-reproducibility/model_bit_diff.py
@@ -51,7 +51,7 @@ plt.close('all')
 ##                          User Inputs                          ##  
 ################################################################### 
 
-vns = ['NH4'] # u, v, w, NO3, NH4 oxygen
+vns = ['NH4'] #['NH4','NO3','oxygen','u','v','w','salt','temp'] # u, v, w, NO3, NH4 oxygen
 date = '2012.10.07'
 
 filetype = 'ocean_avg_0001.nc'
@@ -61,10 +61,11 @@ filetype = 'ocean_avg_0001.nc'
 ##          load output folder, grid data, model output          ##  
 ################################################################### 
 
-# model1 = 'cas7_newtraps00_debugx11ab'
+model1 = 'cas7_newtraps00_debugx11ab'
 # model2 = 'cas7_newtraps01_debugx11ab'
-model1 = 'cas7_newtraps_x11ab'
-model2 = 'cas7_newtrapsnoN_x11ab'
+model2 = 'cas7_newtraps00noWP_debugx11ab'
+# model1 = 'cas7_newtraps_x11ab'
+# model2 = 'cas7_newtrapsnoN_x11ab'
 
 # where to put output figures
 out_dir = Ldir['LOo'] / 'AL_custom_plots'
@@ -88,7 +89,7 @@ ds_model2 = xr.open_dataset(fp_model2)
 
 
 ###################################################################
-##                     Calculate differences                     ##  
+##                      Binary differences                       ##  
 ################################################################### 
 
 for vn in vns:
@@ -123,8 +124,6 @@ for vn in vns:
         lat = ds_model1['lat_psi']
     else:
         raise ValueError(f"Unknown grid type for variable '{vn}'.")
-    
-    print(list(ds_model1.keys()))
 
     # Compute strict difference (True where different, False where equal)
     diff_mask = (v1 != v2) | (v1.isnull() != v2.isnull())
@@ -138,29 +137,147 @@ for vn in vns:
     plot_data = diff_2d.astype(float)
 
     # Set up colormap: black = 0, lightblue = 1, white = NaN
-    cmap = mcolors.ListedColormap(['lightblue', 'black'])
+    cmap = mcolors.ListedColormap(['paleturquoise', 'black'])
     bounds = [-0.5, 0.5, 1.5]
     norm = mcolors.BoundaryNorm(bounds, cmap.N)
 
-    ###################################################################
-    ##                          Plotting                             ##  
-    ################################################################### 
+    # Plotting -------------------------------------------------- 
 
     # Initialize figure
     fig, ax = plt.subplots(1,1, figsize=(10, 8))
 
     # plot
-    map = plt.pcolormesh(lon, lat, plot_data, cmap=cmap, norm=norm, shading='auto')
-    cbar = plt.colorbar(map, ticks=[0, 1])
-    cbar.ax.set_yticklabels(['No differences', 'Differences'],fontsize=12)
+    plt.pcolormesh(lon, lat, plot_data, cmap=cmap, norm=norm, shading='auto')
+    # cbar = plt.colorbar(map, ticks=[0, 1])
+    # cbar.ax.set_yticklabels(['No differences', 'Differences'],fontsize=12)
+
+    # add West Point location
+    wwtp_fn = Ldir['data'] / 'trapsD01' / 'processed_data'/ 'wwtp_data_wasielewski_etal_2024.nc'
+    wwtp_data_ds = xr.open_dataset(wwtp_fn)
+    WP_lat = wwtp_data_ds.lat.sel(source = wwtp_data_ds.name=='King County West Point WWTP').values
+    WP_lon = wwtp_data_ds.lon.sel(source = wwtp_data_ds.name=='King County West Point WWTP').values
+    ax.scatter(WP_lon,WP_lat,s=80, facecolors='none', edgecolors='deeppink')
 
     # format figure
+    ax.text(0.04, 0.95, 'Differences', color='black', fontweight='bold', fontsize=12,
+            transform=ax.transAxes)
+    ax.text(0.04, 0.92, 'No Differences', color='turquoise', fontweight='bold', fontsize=12,
+            transform=ax.transAxes)
     plt.suptitle('Locations where {} differs between runs at any s-level'.format(vn),fontsize=14,fontweight='bold')
     ax.set_title('{} and {} ({})'.format(model1,model2, filetype))
     plt.xlabel('Lon', fontsize=12)
     plt.ylabel('Lat', fontsize=12)
     pfun.dar(ax)
-    ax.set_ylim([46.5,50])
-    ax.set_xlim([-126.5,-122])
+
+    # # Salish Sea
+    # ax.set_ylim([46.5,50])
+    # ax.set_xlim([-126.5,-122])
+
+    # West Point
+    ax.set_ylim([47.4,48])
+    ax.set_xlim([-122.9,-122.1])
+
     plt.tight_layout()
     plt.show()
+
+###################################################################
+##                    Colormap differences                       ##  
+################################################################### 
+
+for vn in vns:
+
+    # Get data, and get rid of ocean_time dim (because this is at a single time)
+    v1 = ds_model1[vn].squeeze()
+    v2 = ds_model2[vn].squeeze()
+
+    if ('eta_rho' in v1.dims) and ('xi_rho' in v1.dims):
+        h_dims = ('eta_rho', 'xi_rho')
+        lon = ds_model1['lon_rho']
+        lat = ds_model1['lat_rho']
+    elif ('eta_u' in v1.dims) and ('xi_u' in v1.dims):
+        h_dims = ('eta_u', 'xi_u')
+        lon = ds_model1['lon_u']
+        lat = ds_model1['lat_u']
+    elif ('eta_v' in v1.dims) and ('xi_v' in v1.dims):
+        h_dims = ('eta_v', 'xi_v')
+        lon = ds_model1['lon_v']
+        lat = ds_model1['lat_v']
+    elif ('eta_psi' in v1.dims) and ('xi_psi' in v1.dims):
+        h_dims = ('eta_psi', 'xi_psi')
+        lon = ds_model1['lon_psi']
+        lat = ds_model1['lat_psi']
+    else:
+        raise ValueError(f"Unknown grid type for variable '{vn}'.")
+
+    # set bounds
+    if vn in ['NO3','NH4']:
+        vn_name = vn
+        vmin = -0.001
+        vmax =  0.001
+    elif vn in ['u','v','w']:
+        vn_name = vn
+        vmin = -0.00001#-0.01
+        vmax =  0.00001#0.01
+    elif vn == 'oxygen':
+        vn_name = vn
+        vmin = -0.001
+        vmax =  0.001
+    elif vn == 'salt':
+        vn_name = vn
+        vmin = -0.00001
+        vmax =  0.00001
+    elif vn == 'temp':
+        vn_name = vn
+        vmin = -0.00001
+        vmax =  0.00001
+    else:
+        print('vmin and vmax not provided for '+ vn)
+
+    # scale variable
+    scale =  pinfo.fac_dict[vn_name]
+
+    # Get model1 data
+    surf_vn_model1 = ds_model1[vn][0,-1,:,:].values
+    bott_vn_model1 = ds_model1[vn][0,0,:,:].values
+    # Get model2 data
+    surf_vn_model2 = ds_model2[vn][0,-1,:,:].values
+    bott_vn_model2 = ds_model2[vn][0,0,:,:].values
+    # Get difference
+    surf_diff = (surf_vn_model1 - surf_vn_model2) * scale
+    bott_diff = (bott_vn_model1 - bott_vn_model2) * scale
+
+    # Initialize figure
+    fig = plt.figure(figsize=(16,8)) # 15,11 for Puget sound and 18,8 for Salish Sea
+    plt.tight_layout()
+
+    subplotnums = [121,122]
+    stexts = ['Surface','Bottom']
+    values = [surf_diff,bott_diff]
+
+    newcmap = cmocean.tools.crop_by_percent(cmocean.cm.balance_r, 20, which='both', N=None)
+
+    # loop through all of the plots we need to make
+    for i,stext in enumerate(stexts):
+
+        # add water/land
+        ax = fig.add_subplot(subplotnums[i])
+
+        # plot values
+        cs = ax.pcolormesh(lon, lat,values[i],vmin=vmin, vmax=vmax, cmap=newcmap)
+        cbar = fig.colorbar(cs)
+        cbar.ax.tick_params(labelsize=14)
+        cbar.outline.set_visible(False)
+        # format figure
+        # ax.set_xlim([xmin,xmax])
+        # ax.set_ylim([ymin,ymax])
+        ax.set_yticklabels([])
+        ax.set_xticklabels([])
+        ax.axis('off')
+        ax.set_ylim([47.4,48])
+        ax.set_xlim([-122.9,-122.1])
+        ax.scatter(WP_lon,WP_lat,s=80, facecolors='none', edgecolors='deeppink')
+        # pfun.add_coast(ax, color='k')
+        pfun.dar(ax)
+        ax.set_title(vn + ' difference at ' + stext + pinfo.units_dict[vn_name], fontsize=16)
+        fig.suptitle('{} minus {}\n'.format(model1,model2) + date + ' {}'.format(filetype),
+                    fontsize=14, fontweight='bold')
