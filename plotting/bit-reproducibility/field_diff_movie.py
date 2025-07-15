a1c40ea779edfc5105373685c5bda5e3223dfdec
ajleeson <aurorajleeson@gmail.com>
2025-07-15 09:05:44 -0700

    update movie script

--- plotting/bit-reproducibility/field_diff_movie.py
+++ plotting/bit-reproducibility/field_diff_movie.py
@@ -77,8 +77,9 @@ fn_list_noWWTP[0] = Ldir['roms_out'] / 'cas7_ats00noWP_debugx11ab' / 'f2012.10.0
 # add additional files
 if his_num > 25:
     for i in np.linspace(25,his_num,his_num-25+1):
-        fn_list_WWTP.append(Ldir['roms_out'] / 'cas7_ats00_debugx11ab' / 'f2012.10.07' / 'ocean_his_00' +str(i) + '.nc')
-        fn_list_noWWTP.append(Ldir['roms_out'] / 'cas7_ats00noWP_debugx11ab' / 'f2012.10.07' / 'ocean_his_00' +str(i) + '.nc')
+        oceanhis = 'ocean_his_00' +str(i) + '.nc'
+        fn_list_WWTP.append(Ldir['roms_out'] / 'cas7_ats00_debugx11ab' / 'f2012.10.07' / oceanhis)
+        fn_list_noWWTP.append(Ldir['roms_out'] / 'cas7_ats00noWP_debugx11ab' / 'f2012.10.07' / oceanhis)
 
 # vns = ['NH4'] #['NH4','NO3','oxygen','u','v','w','salt','temp'] # u, v, w, NO3, NH4 oxygen
 vn = 'NH4' # one variable at a time
