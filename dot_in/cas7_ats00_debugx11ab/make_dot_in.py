ba8dec0f48a739d7e650fee51c2f0841fd1fc979
ajleeson <aurorajleeson@gmail.com>
2025-07-15 09:24:30 -0700

    bio noise

--- dot_in/cas7_ats00_debugx11ab/make_dot_in.py
+++ dot_in/cas7_ats00_debugx11ab/make_dot_in.py
@@ -139,11 +139,11 @@ D['dt'] = dt
 
 if Ldir['short_roms']:
     print(' --- running short roms')
-    his_interval = 30 * dtsec
+    his_interval = 60 * dtsec
     # D['ntimes'] = int(his_interval/dtsec) # run for his_interval
     # his_interval = dtsec
     # D['ntimes'] = 10 # run for ten time steps
-    D['ntimes'] = 900 # run for 900 time steps
+    D['ntimes'] = 1440 # run for 1440 time steps
 else:
     D['ntimes'] = int(days_to_run*86400/dtsec)
 
