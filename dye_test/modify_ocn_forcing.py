9851a83217fde882885a6fa0dfbc089db74dd7ea
ajleeson <aurorajleeson@gmail.com>
2025-08-22 13:49:39 -0700

    modify ocean forcing with 2 dyes

--- dye_test/modify_ocn_forcing.py
+++ dye_test/modify_ocn_forcing.py
@@ -20,27 +20,51 @@ ds_ini['dye_01'] = xr.zeros_like(ds_ini['temp'])
 ds_ini['dye_01'].attrs['long_name'] = 'Passive tracer dye concentration'
 ds_ini['dye_01'].attrs['units'] = 'kg m-3' 
 
+ds_ini['dye_02'] = xr.zeros_like(ds_ini['temp'])
+ds_ini['dye_02'].attrs['long_name'] = 'Passive tracer dye concentration'
+ds_ini['dye_02'].attrs['units'] = 'kg m-3' 
+
 # Ocean boundary conditions
 # North
 dye_time_north = xr.zeros_like(ds_bry['temp_north']).rename({'temp_time': 'dye_time'})
 ds_bry['dye_north_01'] = dye_time_north
 ds_bry['dye_north_01'].attrs['long_name'] = 'Passive tracer dye northern boundary conditions'
 ds_bry['dye_north_01'].attrs['units'] = 'kg m-3' 
+
+ds_bry['dye_north_02'] = dye_time_north
+ds_bry['dye_north_02'].attrs['long_name'] = 'Passive tracer dye northern boundary conditions'
+ds_bry['dye_north_02'].attrs['units'] = 'kg m-3' 
+
 # South
 dye_time_south = xr.zeros_like(ds_bry['temp_south']).rename({'temp_time': 'dye_time'})
 ds_bry['dye_south_01'] = dye_time_south
 ds_bry['dye_south_01'].attrs['long_name'] = 'Passive tracer dye southern boundary conditions'
 ds_bry['dye_south_01'].attrs['units'] = 'kg m-3' 
+
+ds_bry['dye_south_02'] = dye_time_south
+ds_bry['dye_south_02'].attrs['long_name'] = 'Passive tracer dye southern boundary conditions'
+ds_bry['dye_south_02'].attrs['units'] = 'kg m-3' 
+
 # East
 dye_time_east = xr.zeros_like(ds_bry['temp_east']).rename({'temp_time': 'dye_time'})
 ds_bry['dye_east_01'] = dye_time_east
 ds_bry['dye_east_01'].attrs['long_name'] = 'Passive tracer dye eastern boundary conditions'
 ds_bry['dye_east_01'].attrs['units'] = 'kg m-3' 
+
+ds_bry['dye_east_02'] = dye_time_east
+ds_bry['dye_east_02'].attrs['long_name'] = 'Passive tracer dye eastern boundary conditions'
+ds_bry['dye_east_02'].attrs['units'] = 'kg m-3' 
+
 # West
 dye_time_west = xr.zeros_like(ds_bry['temp_west']).rename({'temp_time': 'dye_time'})
 ds_bry['dye_west_01'] = dye_time_west
 ds_bry['dye_west_01'].attrs['long_name'] = 'Passive tracer dye western boundary conditions'
 ds_bry['dye_west_01'].attrs['units'] = 'kg m-3' 
+
+ds_bry['dye_west_02'] = dye_time_west
+ds_bry['dye_west_02'].attrs['long_name'] = 'Passive tracer dye western boundary conditions'
+ds_bry['dye_west_02'].attrs['units'] = 'kg m-3' 
+
 # Add dye_time coordinate
 ds_bry['dye_time'].attrs = ds_bry['temp_time'].attrs.copy()
 
@@ -49,6 +73,11 @@ dye_time = xr.zeros_like(ds_clm['temp']).rename({'temp_time': 'dye_time'})
 ds_clm['dye_01'] = dye_time
 ds_clm['dye_01'].attrs['long_name'] = 'Passive tracer dye concentration'
 ds_clm['dye_01'].attrs['units'] = 'kg m-3' 
+
+ds_clm['dye_02'] = dye_time
+ds_clm['dye_02'].attrs['long_name'] = 'Passive tracer dye concentration'
+ds_clm['dye_02'].attrs['units'] = 'kg m-3' 
+
 # Add dye_time coordinate
 ds_clm['dye_time'].attrs = ds_clm['temp_time'].attrs.copy()
 
@@ -56,7 +85,7 @@ ds_clm['dye_time'].attrs = ds_clm['temp_time'].attrs.copy()
 ################################################################
 # Save .nc files
 print('Saving new files...')
-ds_ini.to_netcdf(path='../../LO_output/forcing/cas7/f2012.10.07/ocnG00dye/ocean_ini.nc')
-ds_bry.to_netcdf(path='../../LO_output/forcing/cas7/f2012.10.07/ocnG00dye/ocean_bry.nc')
-ds_clm.to_netcdf(path='../../LO_output/forcing/cas7/f2012.10.07/ocnG00dye/ocean_clm.nc')
+ds_ini.to_netcdf(path='../../LO_output/forcing/cas7/f2012.10.07/ocnG00dye2/ocean_ini.nc')
+ds_bry.to_netcdf(path='../../LO_output/forcing/cas7/f2012.10.07/ocnG00dye2/ocean_bry.nc')
+ds_clm.to_netcdf(path='../../LO_output/forcing/cas7/f2012.10.07/ocnG00dye2/ocean_clm.nc')
 
