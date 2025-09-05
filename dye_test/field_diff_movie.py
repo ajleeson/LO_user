2f2c65a9f88b7254876680af837aed83820de52c
ajleeson <aurorajleeson@gmail.com>
2025-09-04 17:42:42 -0700

    check exp decaying dye

--- dye_test/field_diff_movie.py
+++ dye_test/field_diff_movie.py
@@ -38,14 +38,14 @@ model1 = 'cas7_exdye2_x11exdye2'            # Dye 2 decays, Same inputs
 # model1 = 'cas7_twindye_x11twindye'          # Both dyes decay, Different inputs
 # model1 = 'cas7_twindyeduplicate_x11twindye' # Duplicate of above
 
-model2 = 'cas7_exdye2_x11exdye2'            # Dye 2 decays, Same inputs
+# model2 = 'cas7_exdye2_x11exdye2'            # Dye 2 decays, Same inputs
 # model2 = 'cas7_exdye2duplicate_x11exdye2'   # Duplicate of above
-# model2 = 'cas7_twindye_x11twindye'          # Both dyes decay, Different inputs
+model2 = 'cas7_twindye_x11twindye'          # Both dyes decay, Different inputs
 # model2 = 'cas7_twindyeduplicate_x11twindye' # Duplicate of above
 
 # Which variables to compare??
-vn1 = 'dye_01'
-vn2 = 'dye_02'
+vn1 = 'dye_01' # model 1
+vn2 = 'dye_01' # model 2
 
 # USER OPTIONS ----------------------------------------------------
 
@@ -103,232 +103,232 @@ lat_u = grid_ds.lat_u.values
 lon_v = grid_ds.lon_v.values
 lat_v = grid_ds.lat_v.values
 
-# ###################################################################
-# ##                   Binary differences movie                    ##  
-# ################################################################### 
+###################################################################
+##                   Binary differences movie                    ##  
+################################################################### 
 
-# for i,fn_model1 in enumerate(fn_list_model1):
+for i,fn_model1 in enumerate(fn_list_model1):
 
-#     # get model output
-#     fn_model2 = fn_list_model2[i]
-#     ds_model1 = xr.open_dataset(fn_model1)
-#     ds_model2 = xr.open_dataset(fn_model2)
+    # get model output
+    fn_model2 = fn_list_model2[i]
+    ds_model1 = xr.open_dataset(fn_model1)
+    ds_model2 = xr.open_dataset(fn_model2)
 
-#     # Get data, and get rid of ocean_time dim (because this is at a single time)
-#     v1 = ds_model1[vn1].squeeze()
-#     v2 = ds_model2[vn2].squeeze()
+    # Get data, and get rid of ocean_time dim (because this is at a single time)
+    v1 = ds_model1[vn1].squeeze()
+    v2 = ds_model2[vn2].squeeze()
 
-#     # Identify vertical and horizontal dims
-#     if 's_rho' in v1.dims:
-#         vert_dim = 's_rho'
-#     elif 's_w' in v1.dims:
-#         vert_dim = 's_w'
-#     else:
-#         raise ValueError(f"No vertical dimension found for {vn1}")
+    # Identify vertical and horizontal dims
+    if 's_rho' in v1.dims:
+        vert_dim = 's_rho'
+    elif 's_w' in v1.dims:
+        vert_dim = 's_w'
+    else:
+        raise ValueError(f"No vertical dimension found for {vn1}")
 
-#     if ('eta_rho' in v1.dims) and ('xi_rho' in v1.dims):
-#         h_dims = ('eta_rho', 'xi_rho')
-#         lon = ds_model1['lon_rho']
-#         lat = ds_model1['lat_rho']
-#     elif ('eta_u' in v1.dims) and ('xi_u' in v1.dims):
-#         h_dims = ('eta_u', 'xi_u')
-#         lon = ds_model1['lon_u']
-#         lat = ds_model1['lat_u']
-#     elif ('eta_v' in v1.dims) and ('xi_v' in v1.dims):
-#         h_dims = ('eta_v', 'xi_v')
-#         lon = ds_model1['lon_v']
-#         lat = ds_model1['lat_v']
-#     elif ('eta_psi' in v1.dims) and ('xi_psi' in v1.dims):
-#         h_dims = ('eta_psi', 'xi_psi')
-#         lon = ds_model1['lon_psi']
-#         lat = ds_model1['lat_psi']
-#     else:
-#         raise ValueError(f"Unknown grid type for variable '{vn1}'.")
+    if ('eta_rho' in v1.dims) and ('xi_rho' in v1.dims):
+        h_dims = ('eta_rho', 'xi_rho')
+        lon = ds_model1['lon_rho']
+        lat = ds_model1['lat_rho']
+    elif ('eta_u' in v1.dims) and ('xi_u' in v1.dims):
+        h_dims = ('eta_u', 'xi_u')
+        lon = ds_model1['lon_u']
+        lat = ds_model1['lat_u']
+    elif ('eta_v' in v1.dims) and ('xi_v' in v1.dims):
+        h_dims = ('eta_v', 'xi_v')
+        lon = ds_model1['lon_v']
+        lat = ds_model1['lat_v']
+    elif ('eta_psi' in v1.dims) and ('xi_psi' in v1.dims):
+        h_dims = ('eta_psi', 'xi_psi')
+        lon = ds_model1['lon_psi']
+        lat = ds_model1['lat_psi']
+    else:
+        raise ValueError(f"Unknown grid type for variable '{vn1}'.")
 
-#     # Compute strict difference (True where different, False where equal)
-#     diff_mask = (v1 != v2) | (v1.isnull() != v2.isnull())
-#     diff_2d = diff_mask.any(dim=vert_dim)
+    # Compute strict difference (True where different, False where equal)
+    diff_mask = (v1 != v2) | (v1.isnull() != v2.isnull())
+    diff_2d = diff_mask.any(dim=vert_dim)
 
-#     # Mask out locations where both are NaN at all depths
-#     both_nan = v1.isnull() & v2.isnull()
-#     diff_2d = diff_2d.where(~both_nan.any(dim=vert_dim))
+    # Mask out locations where both are NaN at all depths
+    both_nan = v1.isnull() & v2.isnull()
+    diff_2d = diff_2d.where(~both_nan.any(dim=vert_dim))
 
-#     # Convert to numeric: 0 = same, 1 = diff, NaN = both missing
-#     plot_data = diff_2d.astype(float)
+    # Convert to numeric: 0 = same, 1 = diff, NaN = both missing
+    plot_data = diff_2d.astype(float)
 
-#     # Set up colormap: black = 0, lightblue = 1, white = NaN
-#     cmap = mcolors.ListedColormap(['paleturquoise', 'black'])
-#     bounds = [-0.5, 0.5, 1.5]
-#     norm = mcolors.BoundaryNorm(bounds, cmap.N)
+    # Set up colormap: black = 0, lightblue = 1, white = NaN
+    cmap = mcolors.ListedColormap(['paleturquoise', 'black'])
+    bounds = [-0.5, 0.5, 1.5]
+    norm = mcolors.BoundaryNorm(bounds, cmap.N)
 
-#     # Plotting -------------------------------------------------- 
+    # Plotting -------------------------------------------------- 
 
-#     # Initialize figure
-#     fig, ax = plt.subplots(1,1, figsize=(10, 8))
+    # Initialize figure
+    fig, ax = plt.subplots(1,1, figsize=(10, 8))
 
-#     # plot
-#     plt.pcolormesh(lon, lat, plot_data, cmap=cmap, norm=norm, shading='auto')
+    # plot
+    plt.pcolormesh(lon, lat, plot_data, cmap=cmap, norm=norm, shading='auto')
 
-#     # add West Point location
-#     wwtp_fn = Ldir['data'] / 'trapsD01' / 'processed_data'/ 'wwtp_data_wasielewski_etal_2024.nc'
-#     wwtp_data_ds = xr.open_dataset(wwtp_fn)
-#     WP_lat = wwtp_data_ds.lat.sel(source = wwtp_data_ds.name=='King County West Point WWTP').values
-#     WP_lon = wwtp_data_ds.lon.sel(source = wwtp_data_ds.name=='King County West Point WWTP').values
-#     ax.scatter(WP_lon,WP_lat,s=80, facecolors='none', edgecolors='deeppink')
+    # add West Point location
+    wwtp_fn = Ldir['data'] / 'trapsD01' / 'processed_data'/ 'wwtp_data_wasielewski_etal_2024.nc'
+    wwtp_data_ds = xr.open_dataset(wwtp_fn)
+    WP_lat = wwtp_data_ds.lat.sel(source = wwtp_data_ds.name=='King County West Point WWTP').values
+    WP_lon = wwtp_data_ds.lon.sel(source = wwtp_data_ds.name=='King County West Point WWTP').values
+    ax.scatter(WP_lon,WP_lat,s=80, facecolors='none', edgecolors='deeppink')
 
-#     # format figure
-#     ax.text(0.04, 0.95, 'Differences', color='black', fontweight='bold', fontsize=12,
-#             transform=ax.transAxes)
-#     ax.text(0.04, 0.92, 'No Differences', color='turquoise', fontweight='bold', fontsize=12,
-#             transform=ax.transAxes)
-#     # plt.suptitle('Locations where dye_01 and dye_02 differs between runs at any s-level\nWithin same run',
-#     #              fontsize=14,fontweight='bold')
-#     plt.suptitle('Locations where {} and {} differs between runs at any s-level\n{} & {}'.format(vn1,vn2,model1,model2),
-#                  fontsize=14,fontweight='bold')
-#     plt.xlabel('Lon', fontsize=12)
-#     plt.ylabel('Lat', fontsize=12)
-#     pfun.dar(ax)
+    # format figure
+    ax.text(0.04, 0.95, 'Differences', color='black', fontweight='bold', fontsize=12,
+            transform=ax.transAxes)
+    ax.text(0.04, 0.92, 'No Differences', color='turquoise', fontweight='bold', fontsize=12,
+            transform=ax.transAxes)
+    # plt.suptitle('Locations where dye_01 and dye_02 differs between runs at any s-level\nWithin same run',
+    #              fontsize=14,fontweight='bold')
+    plt.suptitle('Locations where {} and {} differs between runs at any s-level\n{} & {}'.format(vn1,vn2,model1,model2),
+                 fontsize=14,fontweight='bold')
+    plt.xlabel('Lon', fontsize=12)
+    plt.ylabel('Lat', fontsize=12)
+    pfun.dar(ax)
 
-#     ax.text(0.7, 0.95, 'Hour {}'.format(i), color='black', fontweight='bold', fontsize=12,
-#             transform=ax.transAxes)
+    ax.text(0.7, 0.95, 'Hour {}'.format(i), color='black', fontweight='bold', fontsize=12,
+            transform=ax.transAxes)
 
-#     # Salish Sea
-#     ax.set_ylim([46.5,50])
-#     ax.set_xlim([-126.5,-122])
+    # Salish Sea
+    ax.set_ylim([46.5,50])
+    ax.set_xlim([-126.5,-122])
 
-#     # # West Point
-#     # ax.set_ylim([47.4,48])
-#     # ax.set_xlim([-122.9,-122.1])
+    # # West Point
+    # ax.set_ylim([47.4,48])
+    # ax.set_xlim([-122.9,-122.1])
 
-#     plt.tight_layout()
+    plt.tight_layout()
 
-#     # prepare a directory for results
-#     nouts = ('0000' + str(i))[-4:]
-#     outname = 'plot_' + nouts + '.png'
-#     outfile = outdir /'binary' / outname
-#     print('Plotting ' + str(fn_model1))
-#     sys.stdout.flush()
-#     plt.savefig(outfile)
-#     plt.close()
+    # prepare a directory for results
+    nouts = ('0000' + str(i))[-4:]
+    outname = 'plot_' + nouts + '.png'
+    outfile = outdir /'binary' / outname
+    print('Plotting ' + str(fn_model1))
+    sys.stdout.flush()
+    plt.savefig(outfile)
+    plt.close()
 
-# # make movie
-# if len(fn_list_model1) > 1:
-#     cmd_list = ['ffmpeg','-r','3','-i', str(outdir / 'binary')+'/plot_%04d.png', '-vcodec', 'libx264',
-#         '-pix_fmt', 'yuv420p', '-crf', '25', str(outdir / 'binary')+'/movie.mp4']
-#     proc = Po(cmd_list, stdout=Pi, stderr=Pi)
-#     stdout, stderr = proc.communicate()
-#     if len(stdout) > 0:
-#         print('\n'+stdout.decode())
-#     if len(stderr) > 0:
-#         print('\n'+stderr.decode())
+# make movie
+if len(fn_list_model1) > 1:
+    cmd_list = ['ffmpeg','-r','3','-i', str(outdir / 'binary')+'/plot_%04d.png', '-vcodec', 'libx264',
+        '-pix_fmt', 'yuv420p', '-crf', '25', str(outdir / 'binary')+'/movie.mp4']
+    proc = Po(cmd_list, stdout=Pi, stderr=Pi)
+    stdout, stderr = proc.communicate()
+    if len(stdout) > 0:
+        print('\n'+stdout.decode())
+    if len(stderr) > 0:
+        print('\n'+stderr.decode())
 
-# ###################################################################
-# ##                    Colormap differences                       ##  
-# ################################################################### 
+###################################################################
+##                    Colormap differences                       ##  
+################################################################### 
 
-# for i,fn_model1 in enumerate(fn_list_model1):
+for i,fn_model1 in enumerate(fn_list_model1):
 
-#     # get model output
-#     fn_model2 = fn_list_model2[i]
-#     ds_model1 = xr.open_dataset(fn_model1)
-#     ds_model2 = xr.open_dataset(fn_model2)
+    # get model output
+    fn_model2 = fn_list_model2[i]
+    ds_model1 = xr.open_dataset(fn_model1)
+    ds_model2 = xr.open_dataset(fn_model2)
 
-#     # Get data, and get rid of ocean_time dim (because this is at a single time)
-#     v1 = ds_model1[vn1].squeeze()
-#     v2 = ds_model2[vn2].squeeze()
+    # Get data, and get rid of ocean_time dim (because this is at a single time)
+    v1 = ds_model1[vn1].squeeze()
+    v2 = ds_model2[vn2].squeeze()
 
-#     if ('eta_rho' in v1.dims) and ('xi_rho' in v1.dims):
-#         h_dims = ('eta_rho', 'xi_rho')
-#         lon = ds_model1['lon_rho']
-#         lat = ds_model1['lat_rho']
-#     elif ('eta_u' in v1.dims) and ('xi_u' in v1.dims):
-#         h_dims = ('eta_u', 'xi_u')
-#         lon = ds_model1['lon_u']
-#         lat = ds_model1['lat_u']
-#     elif ('eta_v' in v1.dims) and ('xi_v' in v1.dims):
-#         h_dims = ('eta_v', 'xi_v')
-#         lon = ds_model1['lon_v']
-#         lat = ds_model1['lat_v']
-#     elif ('eta_psi' in v1.dims) and ('xi_psi' in v1.dims):
-#         h_dims = ('eta_psi', 'xi_psi')
-#         lon = ds_model1['lon_psi']
-#         lat = ds_model1['lat_psi']
-#     else:
-#         raise ValueError(f"Unknown grid type for variable '{vn1}'.")
+    if ('eta_rho' in v1.dims) and ('xi_rho' in v1.dims):
+        h_dims = ('eta_rho', 'xi_rho')
+        lon = ds_model1['lon_rho']
+        lat = ds_model1['lat_rho']
+    elif ('eta_u' in v1.dims) and ('xi_u' in v1.dims):
+        h_dims = ('eta_u', 'xi_u')
+        lon = ds_model1['lon_u']
+        lat = ds_model1['lat_u']
+    elif ('eta_v' in v1.dims) and ('xi_v' in v1.dims):
+        h_dims = ('eta_v', 'xi_v')
+        lon = ds_model1['lon_v']
+        lat = ds_model1['lat_v']
+    elif ('eta_psi' in v1.dims) and ('xi_psi' in v1.dims):
+        h_dims = ('eta_psi', 'xi_psi')
+        lon = ds_model1['lon_psi']
+        lat = ds_model1['lat_psi']
+    else:
+        raise ValueError(f"Unknown grid type for variable '{vn1}'.")
 
-#     # set bounds
-#     vmin = -0.00001
-#     vmax =  0.00001
+    # set bounds
+    vmin = -0.00001
+    vmax =  0.00001
 
-#     # Get model1 data
-#     surf_vn_model1 = ds_model1[vn1][0,-1,:,:].values
-#     bott_vn_model1 = ds_model1[vn1][0,0,:,:].values
-#     # Get model2 data
-#     surf_vn_model2 = ds_model2[vn2][0,-1,:,:].values
-#     bott_vn_model2 = ds_model2[vn2][0,0,:,:].values
-#     # Get difference
-#     surf_diff = (surf_vn_model1 - surf_vn_model2)
-#     bott_diff = (bott_vn_model1 - bott_vn_model2)
+    # Get model1 data
+    surf_vn_model1 = ds_model1[vn1][0,-1,:,:].values
+    bott_vn_model1 = ds_model1[vn1][0,0,:,:].values
+    # Get model2 data
+    surf_vn_model2 = ds_model2[vn2][0,-1,:,:].values
+    bott_vn_model2 = ds_model2[vn2][0,0,:,:].values
+    # Get difference
+    surf_diff = (surf_vn_model1 - surf_vn_model2)
+    bott_diff = (bott_vn_model1 - bott_vn_model2)
 
-#     # Initialize figure
-#     fig = plt.figure(figsize=(16,8)) # 15,11 for Puget sound and 18,8 for Salish Sea
-#     plt.tight_layout()
+    # Initialize figure
+    fig = plt.figure(figsize=(16,8)) # 15,11 for Puget sound and 18,8 for Salish Sea
+    plt.tight_layout()
 
-#     subplotnums = [121,122]
-#     stexts = ['Surface','Bottom']
-#     values = [surf_diff,bott_diff]
+    subplotnums = [121,122]
+    stexts = ['Surface','Bottom']
+    values = [surf_diff,bott_diff]
 
-#     newcmap = cmocean.tools.crop_by_percent(cmocean.cm.balance_r, 20, which='both', N=None)
+    newcmap = cmocean.tools.crop_by_percent(cmocean.cm.balance_r, 20, which='both', N=None)
 
-#     # loop through all of the plots we need to make
-#     for j,stext in enumerate(stexts):
+    # loop through all of the plots we need to make
+    for j,stext in enumerate(stexts):
 
-#         # add water/land
-#         ax = fig.add_subplot(subplotnums[j])
+        # add water/land
+        ax = fig.add_subplot(subplotnums[j])
 
-#         # plot values
-#         cs = ax.pcolormesh(lon, lat,values[j],vmin=vmin, vmax=vmax, cmap=newcmap)
-#         cbar = fig.colorbar(cs)
-#         cbar.ax.tick_params(labelsize=14)
-#         cbar.outline.set_visible(False)
-#         # format figure
-#         # ax.set_xlim([xmin,xmax])
-#         # ax.set_ylim([ymin,ymax])
-#         ax.set_yticklabels([])
-#         ax.set_xticklabels([])
-#         ax.axis('off')
-#         ax.set_ylim([47.4,48])
-#         ax.set_xlim([-122.9,-122.1])
-#         ax.scatter(WP_lon,WP_lat,s=80, facecolors='none', edgecolors='deeppink')
-#         # pfun.add_coast(ax, color='k')
-#         pfun.dar(ax)
-#         ax.set_title('Dye difference at ' + stext + '[kg/m3]', fontsize=16)
-#         fig.suptitle('{} minus {}\nBetween {} & {}'.format(vn1,vn2,model1,model2) + d0,
-#                     fontsize=11, fontweight='bold')
+        # plot values
+        cs = ax.pcolormesh(lon, lat,values[j],vmin=vmin, vmax=vmax, cmap=newcmap)
+        cbar = fig.colorbar(cs)
+        cbar.ax.tick_params(labelsize=14)
+        cbar.outline.set_visible(False)
+        # format figure
+        # ax.set_xlim([xmin,xmax])
+        # ax.set_ylim([ymin,ymax])
+        ax.set_yticklabels([])
+        ax.set_xticklabels([])
+        ax.axis('off')
+        ax.set_ylim([47.4,48])
+        ax.set_xlim([-122.9,-122.1])
+        ax.scatter(WP_lon,WP_lat,s=80, facecolors='none', edgecolors='deeppink')
+        # pfun.add_coast(ax, color='k')
+        pfun.dar(ax)
+        ax.set_title('Dye difference at ' + stext + '[kg/m3]', fontsize=16)
+        fig.suptitle('{} minus {}\nBetween {} & {}'.format(vn1,vn2,model1,model2) + d0,
+                    fontsize=11, fontweight='bold')
         
-#         if j == 1:
-#             ax.text(0.6, 0.1, 'Hour {}'.format(i), color='black', fontweight='bold', fontsize=12,
-#             transform=ax.transAxes)
+        if j == 1:
+            ax.text(0.6, 0.1, 'Hour {}'.format(i), color='black', fontweight='bold', fontsize=12,
+            transform=ax.transAxes)
         
-#     # prepare a directory for results
-#     nouts = ('0000' + str(i))[-4:]
-#     outname = 'plot_' + nouts + '.png'
-#     outfile = outdir /'pcolormesh' / outname
-#     print('Plotting ' + str(fn_model1))
-#     sys.stdout.flush()
-#     plt.savefig(outfile)
-#     plt.close()
+    # prepare a directory for results
+    nouts = ('0000' + str(i))[-4:]
+    outname = 'plot_' + nouts + '.png'
+    outfile = outdir /'pcolormesh' / outname
+    print('Plotting ' + str(fn_model1))
+    sys.stdout.flush()
+    plt.savefig(outfile)
+    plt.close()
 
-# # make movie
-# if len(fn_list_model1) > 1:
-#     cmd_list = ['ffmpeg','-r','3','-i', str(outdir / 'pcolormesh')+'/plot_%04d.png', '-vcodec', 'libx264',
-#         '-pix_fmt', 'yuv420p', '-crf', '25', str(outdir / 'pcolormesh')+'/movie.mp4']
-#     proc = Po(cmd_list, stdout=Pi, stderr=Pi)
-#     stdout, stderr = proc.communicate()
-#     if len(stdout) > 0:
-#         print('\n'+stdout.decode())
-#     if len(stderr) > 0:
-#         print('\n'+stderr.decode())
+# make movie
+if len(fn_list_model1) > 1:
+    cmd_list = ['ffmpeg','-r','3','-i', str(outdir / 'pcolormesh')+'/plot_%04d.png', '-vcodec', 'libx264',
+        '-pix_fmt', 'yuv420p', '-crf', '25', str(outdir / 'pcolormesh')+'/movie.mp4']
+    proc = Po(cmd_list, stdout=Pi, stderr=Pi)
+    stdout, stderr = proc.communicate()
+    if len(stdout) > 0:
+        print('\n'+stdout.decode())
+    if len(stderr) > 0:
+        print('\n'+stderr.decode())
 
 ###################################################################
 ##                         Residual plot                         ##  
@@ -491,27 +491,6 @@ expected_dye02 = Q*C/a * (1-np.exp(-1*a*time))
 ax.plot(time, expected_dye01, color='crimson',label='Expected {}'.format(vn1))
 ax.plot(time, expected_dye02, color='navy',label='Expected {}'.format(vn2))
 
-
-# # expected decay
-# time = np.linspace(0,86500,1000)
-# expected = np.exp(-1*1e-5*time)
-# ax.plot(time,expected,color='black',linewidth=3,
-#         label='Expected exponential decay')
-
-# # Fit decay
-# time = np.linspace(0,86500,1000)
-# # Convert to arrays and automatically remove NaNs/Infs
-# mask = np.isfinite(all_sec) & np.isfinite(dye_ratios)
-# all_sec_arr = np.array(all_sec)[mask]
-# dye_ratios_arr = np.array(dye_ratios)[mask]
-# # Fit exponential decay with A=1
-# def exp_decay_fixedA(t, k):
-#     return np.exp(-k * t)
-# k_fit, _ = curve_fit(exp_decay_fixedA, all_sec_arr, dye_ratios_arr, p0=[1e-5])
-# # Fitted curve
-# fit = np.exp(-k_fit * time)
-# ax.plot(time,fit,color='royalblue',linewidth=3,
-#         linestyle='--',label='Fitted exponential decay')
 
 ax.set_ylim(0,3.5e6)
 ax.set_xlim(0,86400)
