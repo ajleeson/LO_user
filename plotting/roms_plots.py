996781b76b545fd9d49b13a209c08253eb7fb3dd
ajleeson <aurorajleeson@gmail.com>
2026-02-21 10:25:53 -1000

    change salt vlim

--- plotting/roms_plots.py
+++ plotting/roms_plots.py
@@ -5541,7 +5541,7 @@ def P_sect_salt(in_dict):
         lon = G['lon_rho'][0,:]
         lat = G['lat_rho'][:,0]
 
-        zdeep = -100
+        zdeep = -3000 # -100
         x_ps = -125.3
         y_ps = 46.5
 
