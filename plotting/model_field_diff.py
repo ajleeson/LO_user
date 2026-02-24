7dbb3827c3e76aa179847e09dd07d6fe9f33581c
ajleeson <aurorajleeson@gmail.com>
2026-02-24 09:54:19 -1000

    dye tests

--- plotting/model_field_diff.py
+++ plotting/model_field_diff.py
@@ -50,13 +50,14 @@ plt.close('all')
 ##                          User Inputs                          ##  
 ################################################################### 
 
-vns = ['salt','u','temp','DIN']#['DIN','u','v','oxygen'] # u, v, w, DIN
+vns = ['salt','u', 'v','temp']#['DIN','u','v','oxygen'] # u, v, w, DIN
 # date = '2013.04.04'
 # date = '2012.10.31'
 # date = '2012.10.07'
 # date = '2014.01.09'
 # date = '2018.06.12'
-date = '2017.02.12'
+# date = '2017.02.12'
+date = '2020.05.31'
 
 ###################################################################
 ##          load output folder, grid data, model output          ##  
@@ -64,8 +65,12 @@ date = '2017.02.12'
 
 # gtagex_longhindcast = 'cas7_t0noN_x4b'
 # gtagex_noN = 'cas7_t0noN_x4b_perf10'
-gtagex_longhindcast = 'cas7_t1_x11ab'#'cas7_t1_x11ab'
-gtagex_noN = 'cas7_t1_x11b'#'cas7_t1noDIN_x11ab'
+
+# gtagex_longhindcast = 'cas7_t1_x11ab'#'cas7_t1_x11ab'
+# gtagex_noN = 'cas7_t1_x11b'#'cas7_t1noDIN_x11ab'
+
+gtagex_longhindcast = 'cas7_t1d_x11ad'#'cas7_t1_x11ab'
+gtagex_noN = 'cas7_t1noDIN_x11ab'#'cas7_t1noDIN_x11ab'
 
 # where to put output figures
 out_dir = Ldir['LOo'] / 'AL_custom_plots'
@@ -115,7 +120,7 @@ ymax = 48.45
 # get model output
 # fp_hindcast = Ldir['roms_out'] / gtagex_longhindcast / ('f'+date) / 'ocean_his_0025.nc'
 # fp_noN = Ldir['roms_out'] / gtagex_noN / ('f'+date) / 'ocean_his_0025.nc'
-fp_hindcast = Ldir['roms_out'] / gtagex_longhindcast / ('f'+date) / 'ocean_his_0001.nc'
+fp_hindcast = Ldir['roms_out'] / gtagex_longhindcast / ('f'+date+'_192cores') / 'ocean_his_0001.nc'
 fp_noN = Ldir['roms_out'] / gtagex_noN / ('f'+date) / 'ocean_his_0001.nc'
 ds_hindcast = xr.open_dataset(fp_hindcast)
 ds_noN = xr.open_dataset(fp_noN)
