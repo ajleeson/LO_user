699f69b6edd8c7d257ef50724909cc1040dae926
ajleeson <aurorajleeson@gmail.com>
2026-04-22 09:03:09 -0700

    tides poster

---
+++ chapter_3/site_selection/model_bathymetry.py
@@ -0,0 +1,169 @@
+"""
+Code to draw plot regional bathymetry
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+import xarray as xr
+from matplotlib.ticker import MaxNLocator
+import cmocean
+import cmcrameri.cm as cmc
+import pandas as pd
+from lo_tools import Lfun, zfun, zrfun
+from lo_tools import plotting_functions as pfun
+Ldir = Lfun.Lstart()
+
+
+# Get LiveOcean grid info --------------------------------------------------
+
+# get the grid data
+ds = xr.open_dataset('../../../LO_data/grids/cas7/grid.nc')
+z = -ds.h.values
+mask_rho = np.transpose(ds.mask_rho.values)
+lon = ds.lon_rho.values
+lat = ds.lat_rho.values
+X = lon[0,:] # grid cell X values
+Y = lat[:,0] # grid cell Y values
+plon, plat = pfun.get_plon_plat(lon,lat)
+# make a version of z with nans where masked
+zm = z.copy()
+zm[np.transpose(mask_rho) == 0] = np.nan
+depth = -zm
+
+# Create bathymetry plot --------------------------------------------------------------
+
+# Initialize figure
+plt.close('all')
+fs = 10
+pfun.start_plot(fs=fs, figsize=(12.3,8.5))
+fig = plt.figure()
+plt.subplots_adjust(wspace=0, hspace=0)
+# create colormap
+# newcmap = cmocean.tools.crop_by_percent(cmocean.cm.deep, 20, which='min')
+newcmap = cmc.tokyo_r
+
+# Model Domain ----------------------------------------------------------
+ax0 = fig.add_subplot(1,3,1)
+# cs = ax0.pcolormesh(plon, plat, zm, vmin=-5, vmax=0, cmap=newcmap)
+cs = ax0.pcolormesh(plon, plat, depth, vmin=0, vmax=800, cmap=newcmap)
+cbar_ax = fig.add_axes([0.117, 0.14, 0.515, 0.02])
+cbar = plt.colorbar(cs, cax=cbar_ax, orientation='horizontal')
+cbar.ax.tick_params(labelsize=18)
+cbar.set_label('Depth [m]', fontsize=18)
+cbar.outline.set_visible(False)
+pfun.dar(ax0)
+pfun.add_coast(ax0, color='silver')
+ax0.set_xlim(-130, -121.8)
+ax0.set_ylim(42, 52)
+
+# plt.xticks(rotation=30, color='gray')
+plt.xticks(rotation=30,horizontalalignment='right',fontsize=18)
+plt.yticks(fontsize=18, rotation=30)
+ax0.set_ylabel('Latitude',  fontsize=14)
+ax0.set_xlabel('Longitude', fontsize=14)
+ax0.xaxis.set_major_locator(MaxNLocator(integer=True))
+ax0.yaxis.set_major_locator(MaxNLocator(integer=True))
+ax0.tick_params(axis='both', labelrotation=45, labelsize=12)
+# add title
+ax0.set_title('(a) Model Domain',fontsize=20, loc='left', fontweight='bold')#,color='#EEEEEE')
+# add distance bar
+lat0 = 42.6
+lon0 = -123.6
+lat1 = lat0
+lon1 = -122.988875
+distances_m = zfun.ll2xy(lon1,lat1,lon0,lat0)
+x_dist_km = round(distances_m[0]/1000)
+ax0.plot([lon0,lon1],[lat0,lat1],color='black',linewidth=1, marker='|')
+ax0.text((lon0+lon1)/2,lat0+0.15,'{} km'.format(x_dist_km),color='k',fontsize=12,
+         horizontalalignment='center')
+
+# draw box around study domain
+bordercolor = 'deeppink'#'#EEEEEE'
+ax0.add_patch(Rectangle((-126, 45.5), 4, 5,
+             edgecolor = bordercolor, facecolor='none', lw=1))
+
+# Study Domain ----------------------------------------------------------
+ax1 = fig.add_subplot(1,3,2)
+
+cs = ax1.pcolormesh(plon, plat, depth, vmin=0, vmax=800, cmap=newcmap)
+
+# format figure
+pfun.dar(ax1)
+
+# Set axis limits
+ax1.set_xlim([-126,-122])
+ax1.set_ylim([45.5,50.5])
+ax1.set_yticklabels([])
+ax1.set_xticklabels([])
+# add title
+ax1.set_title('(b) Study Domain',fontsize=20,loc='left', fontweight='bold')# color='#EEEEEE')
+pfun.add_coast(ax1, color='silver')
+
+# draw box around Puget Sound
+bordercolor = 'deeppink'#'#EEEEEE'
+ax1.add_patch(Rectangle((-123.3, 46.93), 1.2, 1.52,
+             edgecolor = bordercolor, facecolor='none', lw=1))
+
+# add distance bar
+lat0 = 45.8
+lon0 = -123.63425
+lat1 = lat0
+lon1 = -122.988875
+distances_m = zfun.ll2xy(lon1,lat1,lon0,lat0)
+x_dist_km = round(distances_m[0]/1000)
+ax1.plot([lon0,lon1],[lat0,lat1],color='black',linewidth=1, marker='|')
+ax1.text((lon0+lon1)/2,lat0+0.1,'{} km'.format(x_dist_km),color='k',fontsize=12,
+         horizontalalignment='center')
+
+# Puget Sound ----------------------------------------------------------
+ax2 = fig.add_subplot(1,3,3)
+
+# get the grid data
+ds = xr.open_dataset('../../../LO_data/grids/cas7/grid.nc')
+z = -ds.h.values
+mask_rho = np.transpose(ds.mask_rho.values)
+lon = ds.lon_rho.values
+lat = ds.lat_rho.values
+X = lon[0,:] # grid cell X values
+Y = lat[:,0] # grid cell Y values
+plon, plat = pfun.get_plon_plat(lon,lat)
+# make a version of z with nans where masked
+zm = z.copy()
+zm[np.transpose(mask_rho) == 0] = np.nan
+zm[np.transpose(mask_rho) != 0] = -1
+
+# Create map
+cs = ax2.pcolormesh(plon, plat, depth, vmin=0, vmax=250, cmap=newcmap)
+cbar_ax = fig.add_axes([0.65, 0.14, 0.24, 0.02])
+cbar = plt.colorbar(cs, cax=cbar_ax, orientation='horizontal')
+cbar.ax.tick_params(labelsize=18)
+cbar.set_label('Depth [m]', fontsize=18)
+cbar.outline.set_visible(False)
+pfun.add_coast(ax2, color='silver')
+
+# format figure
+pfun.dar(ax2)
+# Set axis limits
+ax2.set_xlim(-123.2, -122.1)
+ax2.set_ylim(46.95, 48.4)
+ax2.set_yticklabels([])
+ax2.set_xticklabels([])
+# add title
+ax2.set_title('(c) Puget Sound',fontsize=20,loc='left', fontweight='bold')# color='#EEEEEE')
+
+# add distance bar
+lat0 = 47.05
+lon0 = -122.38208
+lat1 = lat0
+lon1 = -122.25
+distances_m = zfun.ll2xy(lon1,lat1,lon0,lat0)
+x_dist_km = round(distances_m[0]/1000)
+ax2.plot([lon0,lon1],[lat0,lat1],color='black',linewidth=1, marker='|')
+ax2.text((lon0+lon1)/2,lat0+0.02,'{} km'.format(x_dist_km),color='k',fontsize=12,
+         horizontalalignment='center')
+
+
+# plt.tight_layout()
+plt.subplots_adjust(wspace = 0, bottom=0.26)
+# plt.savefig(out_dir / ('model_bathy.png'))#,transparent='True')
--- chapter_3/site_selection/site_property_maps.py
+++ chapter_3/site_selection/site_property_maps.py
@@ -54,7 +54,7 @@ lat_u = grid_ds.lat_u.values
 lon_v = grid_ds.lon_v.values
 lat_v = grid_ds.lat_v.values
 
-# lon/lat limits (Puget Sound)
+# lon/lat limits (Study Domain)
 xmin = -126
 xmax = -122
 ymin = 45.5
@@ -67,302 +67,165 @@ ds_dye = xr.open_dataset(fp)
 # get surface mixed layer depth
 fp = Ldir['LOo'] / 'extract' / 'cas7_t1_x11ab' / 'sml_plus' / 'threshold_p125' / 'LO_domain_2020.01.01_2020.12.31' / 'LO_domain_sml_plus_2020.01.01_2020.12.31.nc'
 ds_sml = xr.open_dataset(fp)
-print(ds_sml['zSML'])
 ds_sml_may22 = ds_sml.sel(ocean_time=date_formatted)
+# print(ds_sml_may22)
+
+# get wind speeds
+fp = Ldir['LOo'] / 'forcing' / 'cas7' / ('f'+date) / 'atm00'
+ds_Uwind = xr.open_dataset(fp/'Uwind.nc') # wind data is hourly
+ds_Vwind = xr.open_dataset(fp/'Vwind.nc') # wind data is hourly
+# calculate hourly wind speed
+hourly_windspeed = np.sqrt(ds_Uwind['Uwind']**2 + ds_Vwind['Vwind']**2)
+# average to get daily average wind speed
+windspeed_daily = np.nanmean(hourly_windspeed, axis=0)
+# apply land mask
+mask_rho = grid_ds.mask_rho.values   
+
+# get pH in surface mixed layer
+fp = Ldir['LOo'] / 'extract' / 'cas7_t1_x11ab' / 'sml_plus' / 'threshold_p125' / 'pH' / 'pH_2020_sml_p125.nc'
+ds_pH = xr.open_dataset(fp)
+
 
 ###################################################################
-##                       Plot surface dye                        ##  
+##                     Get values to plot                        ##  
 ################################################################### 
 
-px, py = pfun.get_plon_plat(lon,lat)
-vmin = 0
-vmax = 0.5
-
-# surface dye concentration
+# get pcolormesh values
+# surface dye
 surf_dye_01 = ds_dye['dye_01'][0,-1,:,:].values
-
-# Initialize figure
-fig,ax = plt.subplots(1,1, figsize=(7,9))
-
-# plot values
-cmap = copy.copy(plt.get_cmap('plasma'))
-cmap.set_bad(color='white', alpha=1.0)
-cmap = cmc.acton
-cs = ax.pcolormesh(px,py,surf_dye_01,vmin=vmin, vmax=vmax, cmap=cmap)
-
-# Add Puget Sound Inset
-# [x0, y0, width, height]
-axins = ax.inset_axes([0.71, 0.012, 0.45, 0.6])
-# plot values in inset
-axins.pcolormesh(px, py, surf_dye_01, vmin=vmin, vmax=vmax, cmap=cmap)
-# Puget Sound limits
-axins.set_xlim(-123.2, -122.1)
-axins.set_ylim(46.95, 48.4)
-axins.tick_params(left=False, bottom=False)
-# format
-axins.set_xticklabels([])
-axins.set_yticklabels([])
-for spine in axins.spines.values():
-    spine.set_edgecolor('grey')
-    spine.set_linewidth(2)
-
-# add colorbar
-cbar = fig.colorbar(cs, ax=ax, location='bottom', shrink=0.7, pad=0.03)
-cbar.ax.tick_params(labelsize=18, rotation=30)
-cbar.outline.set_visible(False)
-
-# format figure
-ax.set_xlim([xmin,xmax])
-ax.set_ylim([ymin,ymax])
-ax.set_yticklabels([])
-ax.set_xticklabels([])
-ax.tick_params(left=False, bottom=False)
-# pfun.add_coast(ax, color='k')
-pfun.dar(ax)
-pfun.dar(axins)
-ax.set_title('Surface dye\n'+ r'concentration [kg m$^3$]', fontsize=20,
-             loc='Left', fontweight='bold')
-# ax.text()
-
-# Generate plot
-plt.tight_layout
-plt.subplots_adjust(bottom=0.001, top=0.9)
-plt.show()
-
-###################################################################
-##                Plot surface mixed layer depth                 ##  
-################################################################### 
-
-px, py = pfun.get_plon_plat(lon,lat)
-vmin = 0
-vmax = 35
-
-# surface dye concentration
+# surface mixed layer depth
 sml = ds_sml_may22['SML_thickness'][0,:,:].values
-
-# Initialize figure
-fig,ax = plt.subplots(1,1, figsize=(7,9))
-
-# plot values
-# cmap = copy.copy(plt.get_cmap('viridis_r'))
-# cmap.set_bad(color='white', alpha=1.0)
-cmap = cmc.batlow
-cs = ax.pcolormesh(px,py,sml,vmin=vmin, vmax=vmax, cmap=cmap)
-
-# Add Puget Sound Inset
-# [x0, y0, width, height]
-axins = ax.inset_axes([0.71, 0.012, 0.45, 0.6])
-# plot values in inset
-axins.pcolormesh(px, py, sml, vmin=vmin, vmax=vmax, cmap=cmap)
-# Puget Sound limits
-axins.set_xlim(-123.2, -122.1)
-axins.set_ylim(46.95, 48.4)
-axins.tick_params(left=False, bottom=False)
-# format
-axins.set_xticklabels([])
-axins.set_yticklabels([])
-for spine in axins.spines.values():
-    spine.set_edgecolor('grey')
-    spine.set_linewidth(2)
-
-# add colorbar
-cbar = fig.colorbar(cs, ax=ax, location='bottom', shrink=0.7, pad=0.03)
-cbar.ax.tick_params(labelsize=18, rotation=30)
-cbar.outline.set_visible(False)
-
-# format figure
-ax.set_xlim([xmin,xmax])
-ax.set_ylim([ymin,ymax])
-ax.set_yticklabels([])
-ax.set_xticklabels([])
-ax.tick_params(left=False, bottom=False)
-# pfun.add_coast(ax, color='k')
-pfun.dar(ax)
-pfun.dar(axins)
-ax.set_title('Surface mixed layer\n'+ r'depth [m]', fontsize=20,
-             loc='Left', fontweight='bold')
-# ax.text()
-
-# Generate plot
-plt.tight_layout
-plt.subplots_adjust(bottom=0.001, top=0.9)
-plt.show()
-
-###################################################################
-##               Plot SALINITY in surface mixed layer            ##  
-################################################################### 
-
-px, py = pfun.get_plon_plat(lon,lat)
-vmin = 25
-vmax = 33
-
-# surface dye concentration
+# salinity in surface mixed layer
 SA = ds_sml_may22['SA'][0,:,:].values
-
-# Initialize figure
-fig,ax = plt.subplots(1,1, figsize=(7,9))
-
-# plot values
-# cmap = copy.copy(plt.get_cmap('viridis_r'))
-# cmap.set_bad(color='white', alpha=1.0)
-cmap = cmc.imola
-cs = ax.pcolormesh(px,py,SA,vmin=vmin, vmax=vmax, cmap=cmap)
-
-# Add Puget Sound Inset
-# [x0, y0, width, height]
-axins = ax.inset_axes([0.71, 0.012, 0.45, 0.6])
-# plot values in inset
-axins.pcolormesh(px, py, SA, vmin=vmin, vmax=vmax, cmap=cmap)
-# Puget Sound limits
-axins.set_xlim(-123.2, -122.1)
-axins.set_ylim(46.95, 48.4)
-axins.tick_params(left=False, bottom=False)
-# format
-axins.set_xticklabels([])
-axins.set_yticklabels([])
-for spine in axins.spines.values():
-    spine.set_edgecolor('grey')
-    spine.set_linewidth(2)
-
-# add colorbar
-cbar = fig.colorbar(cs, ax=ax, location='bottom', shrink=0.7, pad=0.03)
-cbar.ax.tick_params(labelsize=18, rotation=30)
-cbar.outline.set_visible(False)
-
-# format figure
-ax.set_xlim([xmin,xmax])
-ax.set_ylim([ymin,ymax])
-ax.set_yticklabels([])
-ax.set_xticklabels([])
-ax.tick_params(left=False, bottom=False)
-# pfun.add_coast(ax, color='k')
-pfun.dar(ax)
-pfun.dar(axins)
-ax.set_title('Surface mixed layer\n'+ r'abs. salinity [g kg$^{-1}$]', fontsize=20,
-             loc='Left', fontweight='bold')
-# ax.text()
-
-# Generate plot
-plt.tight_layout
-plt.subplots_adjust(bottom=0.001, top=0.9)
-plt.show()
-
-
-###################################################################
-##             Plot TEMPERATURE in surface mixed layer           ##  
-################################################################### 
-
-px, py = pfun.get_plon_plat(lon,lat)
-vmin = 8
-vmax = 16
-
-# surface dye concentration
+# temperature in surface mixed layer
 CT = ds_sml_may22['CT'][0,:,:].values
-
-# Initialize figure
-fig,ax = plt.subplots(1,1, figsize=(7,9))
-
-# plot values
-# cmap = copy.copy(plt.get_cmap('viridis_r'))
-# cmap.set_bad(color='white', alpha=1.0)
-cmap = cmc.lajolla
-cs = ax.pcolormesh(px,py,CT,vmin=vmin, vmax=vmax, cmap=cmap)
-
-# Add Puget Sound Inset
-# [x0, y0, width, height]
-axins = ax.inset_axes([0.71, 0.012, 0.45, 0.6])
-# plot values in inset
-axins.pcolormesh(px, py, CT, vmin=vmin, vmax=vmax, cmap=cmap)
-# Puget Sound limits
-axins.set_xlim(-123.2, -122.1)
-axins.set_ylim(46.95, 48.4)
-axins.tick_params(left=False, bottom=False)
-# format
-axins.set_xticklabels([])
-axins.set_yticklabels([])
-for spine in axins.spines.values():
-    spine.set_edgecolor('grey')
-    spine.set_linewidth(2)
-
-# add colorbar
-cbar = fig.colorbar(cs, ax=ax, location='bottom', shrink=0.7, pad=0.03)
-cbar.ax.tick_params(labelsize=18, rotation=30)
-cbar.outline.set_visible(False)
-
-# format figure
-ax.set_xlim([xmin,xmax])
-ax.set_ylim([ymin,ymax])
-ax.set_yticklabels([])
-ax.set_xticklabels([])
-ax.tick_params(left=False, bottom=False)
-# pfun.add_coast(ax, color='k')
-pfun.dar(ax)
-pfun.dar(axins)
-ax.set_title('Surface mixed layer\n'+ r'cons. temp [$\degree$C]', fontsize=20,
-             loc='Left', fontweight='bold')
-# ax.text()
-
-# Generate plot
-plt.tight_layout
-plt.subplots_adjust(bottom=0.001, top=0.9)
-plt.show()
-
-###################################################################
-##             Plot ALKALINITY in surface mixed layer           ##  
-################################################################### 
-
-px, py = pfun.get_plon_plat(lon,lat)
-vmin = 1000
-vmax = 2500
-
-# surface dye concentration
+# surface wind speed (daily average)
+windspeed_daily = np.ma.masked_where(mask_rho==0, windspeed_daily)
+# pH in surface mixed layer
+pH_may22 = ds_pH['pH'].values[142,:,:]
+# alkalinity in surface mixed layer
 alk = ds_sml_may22['ALK'][0,:,:].values
 
-# Initialize figure
-fig,ax = plt.subplots(1,1, figsize=(7,9))
+# make list of vars
+vars = [surf_dye_01, sml, SA, CT, windspeed_daily, pH_may22, alk]
 
-# plot values
-# cmap = copy.copy(plt.get_cmap('viridis_r'))
-# cmap.set_bad(color='white', alpha=1.0)
-cmap = cmc.devon
-cs = ax.pcolormesh(px,py,alk,vmin=vmin, vmax=vmax, cmap=cmap)
+# list of vmins and vmax
+vmins = [0,     0,  25, 8,  0,  7.5, 1000]
+vmaxs = [0.5,   35, 33, 16, 12, 8.5, 2100]
 
-# Add Puget Sound Inset
-# [x0, y0, width, height]
-axins = ax.inset_axes([0.71, 0.012, 0.45, 0.6])
-# plot values in inset
-axins.pcolormesh(px, py, alk, vmin=vmin, vmax=vmax, cmap=cmap)
-# Puget Sound limits
-axins.set_xlim(-123.2, -122.1)
-axins.set_ylim(46.95, 48.4)
-axins.tick_params(left=False, bottom=False)
-# format
-axins.set_xticklabels([])
-axins.set_yticklabels([])
-for spine in axins.spines.values():
-    spine.set_edgecolor('grey')
-    spine.set_linewidth(2)
+# get colormaps
+cmaps = [cmc.devon_r, cmc.batlowW_r, cmc.nuuk, cmc.roma_r,
+         cmc.davos, cmc.bam, cmc.acton]
 
-# add colorbar
-cbar = fig.colorbar(cs, ax=ax, location='bottom', shrink=0.7, pad=0.03)
-cbar.ax.tick_params(labelsize=18, rotation=30)
-cbar.outline.set_visible(False)
+# titles
+titles = ['Surface dye\n'+r'concentration [kg m$^{-3}$]',
+          'Surface mixed layer\n'+ r'depth [m]',
+          'Surface mixed layer\n'+ r'abs. salinity [g kg$^{-1}$]',
+          'Surface mixed layer\n'+ r'cons. temp [$\degree$C]',
+          'Surface wind\n'+ r'speed [m s$^{-1}$]',
+          'Surface mixed layer\npH',
+          'Surface mixed layer\n'+ r'total alkalinity [meq m$^{-3}$]']
 
-# format figure
-ax.set_xlim([xmin,xmax])
-ax.set_ylim([ymin,ymax])
-ax.set_yticklabels([])
-ax.set_xticklabels([])
-ax.tick_params(left=False, bottom=False)
-# pfun.add_coast(ax, color='k')
-pfun.dar(ax)
-pfun.dar(axins)
-ax.set_title('Surface mixed layer\n'+ r'alkalinity [???]', fontsize=20,
-             loc='Left', fontweight='bold')
-# ax.text()
+# # labels
+# labels = ['(a) ','(b) ', '(c) ', '(d) ', '(e) ']
 
-# Generate plot
-plt.tight_layout
-plt.subplots_adjust(bottom=0.001, top=0.9)
-plt.show()
 
+###################################################################
+##                         Make plots                            ##  
+################################################################### 
+
+px, py = pfun.get_plon_plat(lon,lat)
+
+lons = lon[0,:]
+lats = lat[:,0]
+
+# for testing: print index of grid cell closest to a lat/lon from google maps
+# print(min(range(len(lats)), key=lambda i: abs(lats[i]-48.298809)))
+# print(min(range(len(lons)), key=lambda i: abs(lons[i]+123.005667)))
+
+
+for var, vmin, vmax, cmap, title in zip(vars[::-1], vmins[::-1], vmaxs[::-1], cmaps[::-1], titles[::-1]):
+
+
+    # Initialize figure
+    fig,ax = plt.subplots(1,1, figsize=(7,9))
+
+    # plot values
+    cs = ax.pcolormesh(px,py,var,vmin=vmin, vmax=vmax, cmap=cmap)
+
+    # Add Puget Sound Inset
+    # [x0, y0, width, height]
+    axins = ax.inset_axes([0.71, 0.0, 0.45, 0.6])
+    # plot values in inset
+    axins.pcolormesh(px, py, var, vmin=vmin, vmax=vmax, cmap=cmap)
+    # Puget Sound limits
+    axins.set_xlim(-123.2, -122.1)
+    axins.set_ylim(46.95, 48.4)
+    axins.tick_params(left=False, bottom=False)
+    # format
+    axins.set_xticklabels([])
+    axins.set_yticklabels([])
+    for spine in axins.spines.values():
+        spine.set_edgecolor('grey')
+        spine.set_linewidth(2)
+
+    # add colorbar
+    cbar = fig.colorbar(cs, ax=ax, location='bottom', shrink=0.7, pad=0.03)
+    cbar.ax.tick_params(labelsize=18, rotation=30)
+    cbar.outline.set_visible(False)
+
+    # format figure
+    ax.set_xlim([xmin,xmax])
+    ax.set_ylim([ymin,ymax])
+    ax.set_yticklabels([])
+    ax.set_xticklabels([])
+    ax.tick_params(left=False, bottom=False)
+    pfun.add_coast(ax, color='silver')
+    pfun.add_coast(axins, color='silver')
+    pfun.dar(ax)
+    pfun.dar(axins)
+    ax.set_title(title, fontsize=20,
+                loc='Left', fontweight='bold')
+    
+    print('======================================\n'+title)
+    # add testbed sites
+    testbed_color = 'crimson'
+    testbed_outline = 'pink'
+    testbed_lats = [48.129378, 48.078611, 48.5453, 47.584536, 47.60079806]
+    testbed_lons = [-123.457488, -123.045000, -123.0121, -122.342908, -122.43131423]
+    for lat, lon in zip(testbed_lats, testbed_lons):
+        ax.scatter(   lon, lat, color='None', s=80, edgecolor=testbed_outline, linewidth=4, zorder=5, marker='D')
+        ax.scatter(   lon, lat, color='None', s=80, edgecolor=testbed_color,   linewidth=2, zorder=5, marker='D')
+        axins.scatter(lon, lat, color='None', s=80, edgecolor=testbed_outline, linewidth=4, zorder=5, marker='D')
+        axins.scatter(lon, lat, color='None', s=80, edgecolor=testbed_color,   linewidth=2, zorder=5, marker='D')
+
+    # add selection of natural sites
+    natural_color = 'black'
+    natural_outline = 'white'
+    locations=['Columbia River Plume',
+               'Saratoga Passage',
+               'Hood Canal',
+               'Van Island Coast',
+               'Quadra Island',
+               'S. SoG (Fraser plume)',
+               'S. of San Juan Islands']
+    natural_ys = [464,875,716,1047,1199,1075,898]
+    natural_xs = [360,578,504,186, 233, 468, 513]
+    for x, y, loc in zip(natural_xs, natural_ys, locations):
+        if x in [578,504]: # only plot Whidbey and Hood Canal in inset
+            axins.scatter(lons[x], lats[y], color='None', s=180, edgecolor=natural_outline, linewidth=4, zorder=5)
+            axins.scatter(lons[x], lats[y], color='None', s=180, edgecolor=natural_color,   linewidth=2, zorder=5)
+        else:
+            ax.scatter(   lons[x], lats[y], color='None', s=180, edgecolor=natural_outline, linewidth=4, zorder=5)
+            ax.scatter(   lons[x], lats[y], color='None', s=180, edgecolor=natural_color,   linewidth=2, zorder=5)
+
+        
+        # print values
+        print('{} ({})'.format(np.round(var[y,x],2), loc))
+
+ 
+
+    # Generate plot
+    plt.tight_layout
+    plt.subplots_adjust(bottom=0.001, top=0.9)
+    plt.show()
