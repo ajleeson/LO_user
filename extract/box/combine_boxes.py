ac43484f57ec2bb3f5518cacba603dc51fb5fed0
ajleeson <aurorajleeson@gmail.com>
2026-08-31 11:21:38 -0700

    fix filepath

--- extract/box/combine_boxes.py
+++ extract/box/combine_boxes.py
@@ -12,8 +12,8 @@ Ldir = Lfun.Lstart()
 # Perturbation Run
 
 # get files to concatenate
-file1_pert = Path('../../LO_output/extract/cas7_t1dgeWB_x11abd3monthscont/box/cresst3_2020.06.01_2020.06.30.nc')
-file2_pert = Path('../../LO_output/extract/cas7_t1dgeWB_x11abd/box/cresst3_2020.07.01_2020.10.31.nc')
+file1_pert = Path('../../../LO_output/extract/cas7_t1dgeWB_x11abd3monthscont/box/cresst3_2020.06.01_2020.06.30.nc')
+file2_pert = Path('../../../LO_output/extract/cas7_t1dgeWB_x11abd/box/cresst3_2020.07.01_2020.10.31.nc')
 # where to put output figures
 out_dir = Ldir['LOo'] / 'extract' / 'cresst3_box_extractions'
 Lfun.make_dir(out_dir)
@@ -27,8 +27,8 @@ with xr.open_mfdataset([file1_pert, file2_pert], combine='by_coords') as ds:
 # Baseline Run
 
 # get files to concatenate
-file1_base = Path('../../LO_output/extract/cas7_t1_x11ab/box/cresst3_2020.06.01_2020.08.31.nc')
-file2_base = Path('../../LO_output/extract/cas7_t1_x11ab/box/cresst3_2020.09.01_2020.10.31.nc')
+file1_base = Path('../../../LO_output/extract/cas7_t1_x11ab/box/cresst3_2020.06.01_2020.08.31.nc')
+file2_base = Path('../../../LO_output/extract/cas7_t1_x11ab/box/cresst3_2020.09.01_2020.10.31.nc')
 # where to put output figures
 out_file_pert = 'baseline_testalkdose_2020.06.01_2020.10.31.nc'                                                                       
 
